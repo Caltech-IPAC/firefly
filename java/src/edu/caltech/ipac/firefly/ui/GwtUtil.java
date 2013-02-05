ad996f3a035a4d90f7fed3f1f1bcece2e50bd642
loi <loi@ipac.caltech.edu>
2013-02-05 15:26:49 -0800

    add filters into table's option panel
    Former-commit-id: 911eeaf2931a47670e093ed0c93434f1345d8156
    Former-commit-id: 06c648d8c6e11a6966a558595d57e59c5f2a41c4

--- java/src/edu/caltech/ipac/firefly/ui/GwtUtil.java
+++ java/src/edu/caltech/ipac/firefly/ui/GwtUtil.java
@@ -29,6 +29,8 @@ import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.FileUpload;
 import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
@@ -126,6 +128,39 @@ public class GwtUtil {
 
     public static ShadowedPanel createShadowTitlePanel(Widget content, String title) {
         return createShadowTitlePanel(content, title, null, false);
+    }
+
+    public static HorizontalPanel makeHoriPanel(HasHorizontalAlignment.HorizontalAlignmentConstant halign,
+                                                HasVerticalAlignment.VerticalAlignmentConstant valign,
+                                                Widget... widgets) {
+        HorizontalPanel hp = new HorizontalPanel();
+        for (Widget w : widgets) {
+            hp.add(w);
+        }
+        if (halign != null) {
+            hp.setHorizontalAlignment(halign);
+            hp.setWidth("100%");
+        }
+        if (valign != null) {
+            hp.setVerticalAlignment(valign);
+        }
+        return hp;
+    }
+
+    public static VerticalPanel makeVertPanel(HasHorizontalAlignment.HorizontalAlignmentConstant halign,
+                                                HasVerticalAlignment.VerticalAlignmentConstant valign,
+                                                Widget... widgets) {
+        VerticalPanel vp = new VerticalPanel();
+        for (Widget w : widgets) {
+            vp.add(w);
+        }
+        if (halign != null) {
+            vp.setHorizontalAlignment(halign);
+        }
+        if (valign != null) {
+            vp.setVerticalAlignment(valign);
+        }
+        return vp;
     }
 
     public static Widget centerAlign(Widget w) {
