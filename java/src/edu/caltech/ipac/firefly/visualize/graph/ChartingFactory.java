37625457a6d8ccfc6c08d1e1abc0a2941f104664
tatianag <tatianag@ipac.caltech.edu>
2013-03-11 10:40:43 -0700

    fixed canvas implementation
    Former-commit-id: 15a2d5f9225420def17ee788a1b0db2706684330
    Former-commit-id: ca4b03bfe3ed9b747d72fe20995edddaa85bf5d6

--- java/src/edu/caltech/ipac/firefly/visualize/graph/ChartingFactory.java
+++ java/src/edu/caltech/ipac/firefly/visualize/graph/ChartingFactory.java
@@ -2,14 +2,12 @@ package edu.caltech.ipac.firefly.visualize.graph;
 
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.canvas.dom.client.Context2d;
-import com.google.gwt.dom.client.CanvasElement;
-import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.widgetideas.graphics.client.Color;
 import com.google.gwt.widgetideas.graphics.client.GWTCanvas;
 import com.googlecode.gchart.client.GChartCanvasFactory;
 import com.googlecode.gchart.client.GChartCanvasLite;
-//import edu.caltech.ipac.firefly.util.BrowserUtil;
-//import edu.caltech.ipac.firefly.visualize.draw.Drawer;
+import com.google.gwt.user.client.Element;
 //import edu.caltech.ipac.firefly.visualize.draw.Graphics;
 //import edu.caltech.ipac.firefly.visualize.draw.JSGraphics;
 
@@ -59,14 +57,12 @@ public class ChartingFactory implements GChartCanvasFactory {
     }
 
 
+
 // =====================================================================
 // -------------------- Inner classes --------------------------------
 // =====================================================================
 
     public static class GChartLightCanvasImpl extends GWTCanvas  implements GChartCanvasLite {
-        public void setBackgroundColor(String cssColor) {
-            super.setBackgroundColor(new Color(cssColor));
-        }
 
         public void setFillStyle(String cssColor) {
             super.setFillStyle(new Color(cssColor));
@@ -78,39 +74,58 @@ public class ChartingFactory implements GChartCanvasFactory {
     }
 
 
-    public static class GChartGraphicsCanvasImpl extends Composite implements GChartCanvasLite {
+    public static class GChartGraphicsCanvasImpl extends Widget implements GChartCanvasLite {
 
-        private Canvas canvas= Canvas.createIfSupported();
-        private final CanvasElement cElement= canvas.getCanvasElement();
-        private final Context2d ctx= cElement.getContext2d();
+        Canvas canvas;
+       	Context2d canvasContext;
 
-
-        public GChartGraphicsCanvasImpl () {
-            initWidget(canvas);
-        }
-
-        public void arc(double x, double y, double radius, double startAngle, double endAngle, boolean antiClockwise) {
-            ctx.arc(x,y,radius,startAngle,endAngle,antiClockwise);
-        }
-
-        public void beginPath() { ctx.beginPath(); }
-        public void clear() { ctx.clearRect(0,0,cElement.getWidth(),cElement.getHeight()); }
-        public void closePath() { ctx.closePath(); }
-        public void fill() { ctx.fill();  }
-        public void lineTo(double x, double y) { ctx.lineTo(x,y); }
-        public void moveTo(double x, double y) { ctx.moveTo(x,y);  }
-
-        public void resize(int width, int height) {
-            canvas.setCoordinateSpaceWidth(width);
-            canvas.setCoordinateSpaceHeight(height);
-            cElement.setHeight(height);
-            cElement.setHeight(width);
-        }
-
-        public void setFillStyle(String canvasFillStyle) { ctx.setFillStyle(canvasFillStyle);  }
-        public void setLineWidth(double width) { ctx.setLineWidth(width);  }
-        public void setStrokeStyle(String canvasStrokeStyle) { ctx.setStrokeStyle(canvasStrokeStyle);  }
-        public void stroke() { ctx.stroke();  }
+       	public GChartGraphicsCanvasImpl() {
+       		canvas = Canvas.createIfSupported();
+       		canvasContext = canvas.getContext2d();
+       	}
+       	@Override
+       	public Element getElement() {
+       		return canvas.getElement();
+       	}
+       	public void setStrokeStyle(String cssColor) {
+       		// Sharp angles of default MITER can overwrite adjacent pie slices
+       		canvasContext.setLineJoin(Context2d.LineJoin.ROUND);
+       		canvasContext.setStrokeStyle(cssColor);
+       	}
+       	public void setFillStyle(String cssColor) {
+       		canvasContext.setFillStyle(cssColor);
+       	}
+       	public void arc(double x, double y, double radius, double startAngle, double endAngle, boolean antiClockwise) {
+       		canvasContext.arc(x, y, radius, startAngle, endAngle, antiClockwise);
+       	}
+       	public void beginPath() {
+       		canvasContext.beginPath();
+       	}
+       	public void clear() {
+       		canvasContext.clearRect(0, 0, canvas.getCoordinateSpaceWidth(), canvas.getCoordinateSpaceHeight());
+       	}
+       	public void closePath() {
+       		canvasContext.closePath();
+       	}
+       	public void fill() {
+       		canvasContext.fill();
+       	}
+       	public void lineTo(double x, double y) {
+       		canvasContext.lineTo(x, y);
+       	}
+       	public void moveTo(double x, double y) {
+       		canvasContext.moveTo(x, y);
+       	}
+       	public void resize(int width, int height) {
+       		canvas.setCoordinateSpaceWidth(width);
+       		canvas.setCoordinateSpaceHeight(height);
+       	}
+       	public void setLineWidth(double width) {
+       		canvasContext.setLineWidth(width);
+       	}
+       	public void stroke() {
+       		canvasContext.stroke();
+       	}
     }
 
     /*
