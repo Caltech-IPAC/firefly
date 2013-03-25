82d93352827b7e37b6f01b7ed5fa93e6d7325657
loi <loi@ipac.caltech.edu>
2013-03-25 10:14:42 -0700

    build script changes
    Former-commit-id: 15e735f5b9f44fb73595b0513be2e5ca8d00713f
    Former-commit-id: 01a25bb5db4c3bafbf758d31f23d72da5dde8b21

--- config/hydra_deploy.sh
+++ config/hydra_deploy.sh
@@ -15,93 +15,119 @@ function tomcat() {
 
 function deploy() {
 
-    # default prefix
-    app_prefix=applications#
+    # default naming convention
+    app=$1
+    war_name=applications#$app
 
-    # setup which application to deploy.
+    # check for exceptions
     case "$1" in
-      planck)
-            app=planck
-            ;;
-      wise)
-            app=wise
-            ;;
-      iwise)
-            app=iwise
+      hydra)
+            war_name=hydra
             ;;
       fftools)
-            app=iwise
-            app_prefix=
+            war_name=fftools
             ;;
-      finderchart)
-            app=finderchart
+      heritage)
+            war_name=applications#Spitzer#SHA
             ;;
-      ptf)
-            app=ptf
+      voservices)
+            war_name=applications#Spitzer#VO
             ;;
-      *)
-            echo $"Usage: deploy {planck|wise|fftools|finderchart|ptf}"
-            exit
     esac
-
-    if [ ! -d "${server_dir}/config/${app}" ]; then
-        echo "App directory does not exists: ${server_dir}/config/${app}"
-        exit 1
-    fi
-    if [ ! -d "${server_dir}/repos/${app}/${version}" ]; then
-        echo "Repos directory does not exists: ${server_dir}/repos/${app}/${version}"
-        exit 1
-    fi
 
     tomcat_dir=${server_dir}/nodes/${host_name}
     version=current
-    app_name=${app_prefix}${app}
+    app_name=$war_name
 
+
+    if [ ! -d "${server_dir}/config/${app}" ]; then
+        echo -e "\nERROR!!!! App directory does not exists: ${server_dir}/config/${app}"
+        return
+    fi
+    if [ ! -d "${server_dir}/repos/${app}/${version}" ]; then
+        echo -e "\nERROR!!!! Repos directory does not exists: ${server_dir}/repos/${app}/${version}"
+        return
+    fi
 
 
     cd ${server_dir}/repos/${app}/${version}
-    echo "Deploying   `pwd -P`"
+    echo -e "\nDeploying   `pwd -P`"
     ls -l
-    read -p "\nPress [Enter] key to start."
 
     cd ${server_dir}/config/${app}
-    jar xf ${server_dir}/repos/${app}/${version}/${app_name}-config.jar
+    jar xf ${server_dir}/repos/${app}/${version}/${war_name}-config.jar
 
     cd ${server_dir}/repos/${app}/${version}
 
-    rm -r ${tomcat_dir}1/webapps/${app_name}*
-    rm -r ${tomcat_dir}2/webapps/${app_name}*
-    rm ${tomcat_dir}1/conf/Catalina/localhost/*
-    rm ${tomcat_dir}2/conf/Catalina/localhost/*
+    rm -r ${tomcat_dir}1/webapps/${war_name}*
+    rm -r ${tomcat_dir}2/webapps/${war_name}*
     rm ${tomcat_dir}2/temp/ehcache/${app}/*
     rm ${tomcat_dir}1/temp/ehcache/${app}/*
-    cp ${app_name}.war ${tomcat_dir}1/webapps/
-    cp ${app_name}.war ${tomcat_dir}2/webapps/
+    cp ${war_name}.war ${tomcat_dir}1/webapps/
+    cp ${war_name}.war ${tomcat_dir}2/webapps/
 
 }
 
 #==============================================================================
 #  MAIN
 #==============================================================================
-echo "Stopping Tomcat..."
-tomcat stop
-sleep 5
 
-echo "\nchecking for tomcat process on 1"
-ps -ef | grep org.apache.catalina | cut -c -100
-echo "\nchecking for tomcat process on 2"
-ssh irsadmin@${host_name}2 ps -ef | grep org.apache.catalina | cut -c -100
 
-read -p "\nPress [Enter] key to start deploying."
+if [ "$#" -le "0" ]; then
+     echo $"Usage: deploy.sh [one or more repos directory's name]"
+     exit -1
+fi
 
+dostart=true
+dostop=true
+dodeploy=true
 for i in "$@"
 do
-    deploy "$i"
+    if [ "$i" == "--deploy-only" ]; then
+        dostart=false
+        dostop=false
+        break
+    fi
+    if [ "$i" == "--tomcat-start" ]; then
+        dostop=false
+        dodeploy=false
+        break
+    fi
+    if [ "$i" == "--tomcat-stop" ]; then
+        dostart=false
+        dodeploy=false
+        break
+    fi
 done
 
-read -p "\nPress [Enter] key to start Tomcat."
-echo "Starting Tomcat..."
-tomcat start
 
+if [ "$dostop" == "true" ]; then
+    echo -e "\nStopping Tomcat..."
+    tomcat stop
+    sleep 15
+
+    echo -e "\nChecking for tomcat process on 1"
+    ps -ef | grep org.apache.catalina | cut -c -100
+    echo -e "\nChecking for tomcat process on 2"
+    ssh irsadmin@${host_name}2 ps -ef | grep org.apache.catalina | cut -c -100
+fi
+
+
+if [ "$dodeploy" == "true" ]; then
+    for i in "$@"
+    do
+        if [[ "$i" != "--"* ]]; then
+            deploy "$i"
+        fi
+    done
+fi
+
+
+if [ "$dostart" == "true" ]; then
+    echo -e "\nStarting Tomcat..."
+    tomcat start
+fi
+
+echo -e "\nDeploy completed..."
 
 
