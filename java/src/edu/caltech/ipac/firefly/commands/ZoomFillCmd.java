68f84d9ce5e999452473b8df3e6d85d392293a35
roby <roby@ipac.caltech.edu>
2013-05-10 14:59:22 -0700

    many fftools viewer changes based on feedback
    Former-commit-id: c974f0f5ac9711987b73919e040d2ea69af2fcb0
    Former-commit-id: 3e9c1bb4a65590d0d0b4a371558cbb54605dfa9a

---
+++ java/src/edu/caltech/ipac/firefly/commands/ZoomFillCmd.java
@@ -0,0 +1,19 @@
+package edu.caltech.ipac.firefly.commands;
+
+import com.google.gwt.user.client.ui.Image;
+import edu.caltech.ipac.firefly.resbundle.images.VisIconCreator;
+import edu.caltech.ipac.firefly.visualize.ZoomUtil;
+
+
+public class ZoomFillCmd extends ZoomCmd {
+    public static final String CommandName= "zoomFit";
+
+    public ZoomFillCmd() { super(CommandName, ZoomUtil.FitFill.FIT); }
+
+    @Override
+    protected Image createCmdImage() {
+        VisIconCreator ic= VisIconCreator.Creator.getInstance();
+        return new Image(ic.getZoomFit());
+    }
+
+}
---
+++ java/src/edu/caltech/ipac/firefly/commands/ZoomFitCmd.java
@@ -0,0 +1,27 @@
+package edu.caltech.ipac.firefly.commands;
+
+import com.google.gwt.user.client.ui.Image;
+import edu.caltech.ipac.firefly.resbundle.images.VisIconCreator;
+import edu.caltech.ipac.firefly.visualize.WebPlot;
+
+
+public class ZoomFitCmd extends ZoomCmd {
+    public static final String CommandName= "zoomOriginal";
+
+    public ZoomFitCmd() { super(CommandName,WebPlot.ZDir.ORIGINAL); }
+
+    @Override
+    protected Image createCmdImage() {
+
+        VisIconCreator ic= VisIconCreator.Creator.getInstance();
+        String iStr= this.getIconProperty();
+        if (iStr!=null) {
+
+            if (iStr.equals("zoomOriginal.Icon"))  {
+                return new Image(ic.getZoomOriginal());
+            }
+        }
+        return null;
+    }
+
+}
