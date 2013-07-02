e81dd62c98900cf04d67e3c3f3bf5bab0d3a43e0
tatianag <tatianag@ipac.caltech.edu>
2013-07-02 16:17:46 -0700

    allwisetest - position search in iwise
    Former-commit-id: 17395d2931f182e3797f9fcc73932bec0fcb94e8
    Former-commit-id: cdbfbeee560bf253d5c097cba19cd5eae96d2111

--- java/src/edu/caltech/ipac/firefly/server/query/wise/WiseFileRetrieve.java
+++ java/src/edu/caltech/ipac/firefly/server/query/wise/WiseFileRetrieve.java
@@ -60,6 +60,7 @@ public class WiseFileRetrieve extends URLFileInfoProcessor {
         PROD_LEVEL_MAP.put(WiseRequest.PASS2_3BAND+"|3a", "links-pass2/l3a-3band/");
         PROD_LEVEL_MAP.put(WiseRequest.PASS2_4BAND+"|1b", "links-pass2/l1b-4band/");
         PROD_LEVEL_MAP.put(WiseRequest.PASS2_4BAND+"|3a", "links-pass2/l3a-4band/");
+        PROD_LEVEL_MAP.put(WiseRequest.ALLWISE_TEST+"|3a", "links-allwisetest/l3a/");
     }
 
 
