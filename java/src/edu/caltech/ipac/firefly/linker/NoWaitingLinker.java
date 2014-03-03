5fdabd0c22ff2c3b14244a282d44c03665c18d16
roby <roby@ipac.caltech.edu>
2014-03-03 11:44:49 -0800

    Frontpage script part 2
    Former-commit-id: 04c63b4bc34f0960c91fea1d8ab497eb222531cb
    Former-commit-id: 4cbc3e68353e353fe8dd544b4f5a2eea5bc5dd9a

--- java/src/edu/caltech/ipac/firefly/linker/NoWaitingLinker.java
+++ java/src/edu/caltech/ipac/firefly/linker/NoWaitingLinker.java
@@ -5,33 +5,33 @@ package edu.caltech.ipac.firefly.linker;
  * Time: 3:03 PM
  */
 
+import com.google.gwt.core.ext.linker.LinkerOrder;
+import com.google.gwt.core.ext.linker.Shardable;
+import com.google.gwt.core.linker.CrossSiteIframeLinker;
 
 /**
  * @author Trey Roby
  */
-public class NoWaitingLinker {
+@LinkerOrder(LinkerOrder.Order.PRIMARY)
+@Shardable
+public class NoWaitingLinker extends CrossSiteIframeLinker {
 
 
-//======================================================================
-//----------------------- Constructors ---------------------------------
-//======================================================================
-
-//======================================================================
-//----------------------- Public Methods -------------------------------
-//======================================================================
-
-//=======================================================================
-//-------------- Method from LabelSource Interface ----------------------
-//=======================================================================
-
-//======================================================================
-//------------------ Private / Protected Methods -----------------------
-//======================================================================
-
-
-// =====================================================================
-// -------------------- Factory Methods --------------------------------
-// =====================================================================
+    /**
+     * Returns the name of the {@code JsIsBodyLoaded} script.  By default,
+     * returns {@code "com/google/gwt/core/ext/linker/impl/isBodyLoaded.js"}.
+     *
+     * @param context a LinkerContext
+     */
+//    protected String getJsIsBodyLoaded(LinkerContext context) {
+//        System.out.println("getJsIsBodyLoaded");
+//        return "edu/caltech/ipac/firefly/linker/nowaitIsBodyLoaded.js";
+//    }
+//
+//    @Override
+//    protected boolean shouldInstallCode(LinkerContext context) {
+//        System.out.println("shouldInstallCode returns:"+ super.shouldInstallCode(context));
+//        return super.shouldInstallCode(context);
 
 }
 
