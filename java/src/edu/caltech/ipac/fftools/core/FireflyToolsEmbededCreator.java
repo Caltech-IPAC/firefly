de82d1621440938b891a8e23a0595cf0fb0878cc
tatianag <tatianag@ipac.caltech.edu>
2014-04-21 15:25:16 -0700

    IRSA Viewer embedded, nothing happens when click  help (?) for XY plot, fixes #66
    Former-commit-id: 41afe06ec60f3e7a90837c29617eeb349115763f
    Former-commit-id: 070b15f676d8e4ff7ef883b88e1a666e6a24fc49

--- java/src/edu/caltech/ipac/fftools/core/FireflyToolsEmbededCreator.java
+++ java/src/edu/caltech/ipac/fftools/core/FireflyToolsEmbededCreator.java
@@ -1,16 +1,19 @@
 package edu.caltech.ipac.fftools.core;
 
 import com.google.gwt.user.client.ui.Image;
+import edu.caltech.ipac.firefly.commands.OverviewHelpCmd;
 import edu.caltech.ipac.firefly.core.AlertManager;
 import edu.caltech.ipac.firefly.core.Creator;
 import edu.caltech.ipac.firefly.core.DefaultCreator;
 import edu.caltech.ipac.firefly.core.DefaultRequestHandler;
+import edu.caltech.ipac.firefly.core.GeneralCommand;
 import edu.caltech.ipac.firefly.core.LoginManager;
 import edu.caltech.ipac.firefly.core.RequestHandler;
 import edu.caltech.ipac.firefly.core.layout.LayoutManager;
 import edu.caltech.ipac.firefly.ui.ServerTask;
 import edu.caltech.ipac.firefly.ui.panels.Toolbar;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class FireflyToolsEmbededCreator implements Creator {
@@ -28,10 +31,15 @@ public class FireflyToolsEmbededCreator implements Creator {
 
     public boolean isApplication() { return false; }
 
-
-
     public Toolbar getToolBar() { return null; }
-    public Map makeCommandTable() { return null; }
+
+    public Map makeCommandTable() {
+        HashMap<String, GeneralCommand> commands = new HashMap<String, GeneralCommand>();
+        GeneralCommand cmd = new OverviewHelpCmd();
+        commands.put(cmd.getName(), cmd);
+        return commands;
+    }
+
     public RequestHandler makeCommandHandler() { return new DefaultRequestHandler(); }
     public LoginManager makeLoginManager() { return null; }
     public String getLoadingDiv() { return null; }
