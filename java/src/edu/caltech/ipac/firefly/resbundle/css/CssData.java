662e275cf0b3f3279415d93aa7860a6d5578bfa5
roby <roby@ipac.caltech.edu>
2014-05-06 16:08:28 -0700

    removed a lot of old icons, a other small clean up
    Former-commit-id: 81b8a15d830e581bdad1e6b272fd9f92d252ea9e
    Former-commit-id: c28b55b59c477997fb4e5f7b56c4111ab3437703

--- java/src/edu/caltech/ipac/firefly/resbundle/css/CssData.java
+++ java/src/edu/caltech/ipac/firefly/resbundle/css/CssData.java
@@ -13,28 +13,6 @@ public interface CssData extends ClientBundle {
     @ClientBundle.Source("firefly-bundle.css")
     FireflyCss getFireflyCss();
 
-//    @Source("exp-border-left.png")
-//    @ImageResource.ImageOptions(repeatStyle= ImageResource.RepeatStyle.Vertical)
-//    public ImageResource popupBorderLeft();
-
-
-//    @Source("exp-border-right.png")
-//    @ImageResource.ImageOptions(repeatStyle= ImageResource.RepeatStyle.Vertical, width = 8)
-//    public ImageResource popupBorderRight();
-
-
-//    @Source("exp-border-top.png")
-//    @ImageResource.ImageOptions(repeatStyle= ImageResource.RepeatStyle.Horizontal)
-//    public ImageResource popupBorderTop();
-
-//    @Source("exp-border-bottom.png")
-//    @ImageResource.ImageOptions(repeatStyle= ImageResource.RepeatStyle.Horizontal)
-//    public ImageResource popupBorderBottom();
-
-
-//    @Source("exp-corner-with-shadow.png")
-//    public ImageResource popupCorner();
-
     @Source("tab_bg.png")
     @ImageResource.ImageOptions(repeatStyle= ImageResource.RepeatStyle.Horizontal)
     public ImageResource tabBackground();
