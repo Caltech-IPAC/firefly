429192dd5099d59c41687d7fd3e34f109474051f
loi <loi@ipac.caltech.edu>
2014-05-22 09:20:35 -0700

    adding more caching headers
    Former-commit-id: 5c99287cd8dc8ffc54b6c151950f0b1e3d116fd8
    Former-commit-id: a1bc5e55437a07832be1f56647f4d2e21f5f6691

--- java/src/edu/caltech/ipac/firefly/server/filters/NoCacheFilter.java
+++ java/src/edu/caltech/ipac/firefly/server/filters/NoCacheFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * Date: Jul 9, 2008
@@ -21,10 +22,10 @@ public class NoCacheFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         if (response instanceof HttpServletResponse) {
             HttpServletResponse res = (HttpServletResponse) response;
-            res.setHeader("Cache-Control", "no-cache");
-            res.setHeader("Pragma", "no-cache");
             res.setDateHeader("Expires", 0);
-
+            res.setDateHeader("Last-Modified", new Date().getTime());
+            res.setHeader("Cache-Control", "no-store, no-cache, max-age=0");
+            res.setHeader("Pragma", "no-cache");
         }
         filterChain.doFilter( request, response );
     }
