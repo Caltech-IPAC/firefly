8412f22c04992b04deba8c34473bab8a85da3b7c
tatianag <tatianag@ipac.caltech.edu>
2014-08-14 11:38:29 -0700

    FUSE: using stil.jar library to parse vo tables (for 2mass SIA and cone search)
    Former-commit-id: db886a22c1c23d54d20738ce5b93d36e9f816c94
    Former-commit-id: 6cd2d213822fd12a4540d8f63c402241a8b4db81

--- java/src/edu/caltech/ipac/firefly/server/persistence/QueryByConeSearchURL.java
+++ java/src/edu/caltech/ipac/firefly/server/persistence/QueryByConeSearchURL.java
@@ -1,38 +1,18 @@
 package edu.caltech.ipac.firefly.server.persistence;
 
-import edu.caltech.ipac.astro.IpacTableWriter;
-import edu.caltech.ipac.client.net.URLDownload;
-import edu.caltech.ipac.firefly.core.EndUserException;
-import edu.caltech.ipac.firefly.data.*;
-import edu.caltech.ipac.firefly.data.table.MetaConst;
-import edu.caltech.ipac.firefly.data.table.TableMeta;
-import edu.caltech.ipac.firefly.server.ServerContext;
+import edu.caltech.ipac.firefly.data.CatalogRequest;
+import edu.caltech.ipac.firefly.data.ReqConst;
+import edu.caltech.ipac.firefly.data.TableServerRequest;
+import edu.caltech.ipac.firefly.data.WspaceMeta;
 import edu.caltech.ipac.firefly.server.WorkspaceManager;
-import edu.caltech.ipac.firefly.server.dyn.DynServerUtils;
 import edu.caltech.ipac.firefly.server.query.DataAccessException;
-import edu.caltech.ipac.firefly.server.query.IpacTablePartProcessor;
 import edu.caltech.ipac.firefly.server.query.ParamDoc;
 import edu.caltech.ipac.firefly.server.query.SearchProcessorImpl;
 import edu.caltech.ipac.firefly.server.util.Logger;
 import edu.caltech.ipac.firefly.util.MathUtil;
-import edu.caltech.ipac.util.*;
-import edu.caltech.ipac.util.cache.Cache;
-import edu.caltech.ipac.util.cache.CacheManager;
-import edu.caltech.ipac.util.cache.StringKey;
 import edu.caltech.ipac.visualize.plot.CoordinateSys;
 import edu.caltech.ipac.visualize.plot.Plot;
 import edu.caltech.ipac.visualize.plot.WorldPt;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
-import static edu.caltech.ipac.firefly.util.DataSetParser.DESC_TAG;
-import static edu.caltech.ipac.firefly.util.DataSetParser.makeAttribKey;
 
 /**
  * @author tatianag
@@ -44,7 +24,7 @@ import static edu.caltech.ipac.firefly.util.DataSetParser.makeAttribKey;
          @ParamDoc(name="accessUrl", desc="access URL")
         })
 
-public class QueryByConeSearchURL extends IpacTablePartProcessor {
+public class QueryByConeSearchURL extends QueryVOTABLE {
     private static final Logger.LoggerImpl _log = Logger.getLogger();
 
     public static final String RADIUS_KEY = "radius";
@@ -61,38 +41,14 @@ public class QueryByConeSearchURL extends IpacTablePartProcessor {
         return "conesearch-";
     }
 
-    @Override
-    protected File loadDataFile(TableServerRequest request) throws IOException, DataAccessException {
-
-        long start = System.currentTimeMillis();
-
-        String fromCacheStr = "";
-
-        StringKey key = new StringKey(this.getClass().getName(), getUniqueID(request));
-        Cache cache = CacheManager.getCache(Cache.TYPE_PERM_FILE);
-        File retFile = (File) cache.get(key);
-        if (retFile == null) {
-            retFile = queryVOSearchService(request);  // all the work is done here
-            cache.put(key, retFile);
-        } else {
-            fromCacheStr = "   (from Cache)";
-        }
-
-        long elaspe = System.currentTimeMillis() - start;
-        String sizeStr = FileUtil.getSizeAsString(retFile.length());
-        String timeStr = UTCTimeUtil.getHMSFromMills(elaspe);
-
-        _log.info("catalog: " + timeStr + fromCacheStr,
-                "filename: " + retFile.getPath(),
-                "size:     " + sizeStr);
-
-        return retFile;
-    }
-
     protected String getQueryString(TableServerRequest req) throws DataAccessException {
         String accessUrl = req.getParam(ACCESS_URL);
         if (accessUrl == null) {
             throw new DataAccessException("could not find the parameter "+ACCESS_URL);
+        }
+        if (!(accessUrl.endsWith("?") || accessUrl.endsWith("&"))) {
+            if (accessUrl.contains("?")) { accessUrl += "&"; }
+            else { accessUrl += "?"; }
         }
 
         WorldPt wpt = req.getWorldPtParam(ReqConst.USER_TARGET_WORLD_PT);
@@ -103,134 +59,9 @@ public class QueryByConeSearchURL extends IpacTablePartProcessor {
 
         double radVal = req.getDoubleParam(RADIUS_KEY);
         double radDeg = MathUtil.convert(MathUtil.Units.parse(req.getParam(CatalogRequest.RAD_UNITS), MathUtil.Units.DEGREE), MathUtil.Units.DEGREE, radVal);
-        return accessUrl + "&RA=" + wpt.getLon() + "&DEC=" +wpt.getLat() + "&SR=" + radDeg;
+        return accessUrl + "RA=" + wpt.getLon() + "&DEC=" +wpt.getLat() + "&SR=" + radDeg;
 
     }
-
-    private File queryVOSearchService(TableServerRequest req) throws IOException, DataAccessException {
-
-        try {
-            File votable = getSearchResult(getQueryString(req), getFilePrefix(req));
-            DataGroup[] groups = VoTableUtil.voToDataGroups(votable.getAbsolutePath(), false);
-            if (groups == null || groups.length<1) {
-                throw new EndUserException("cone search query failed", "unable to convert results to data group");
-            }
-            DataGroup dg = groups[0];
-            DataGroup.Attribute raColAttr = dg.getAttribute("POS_EQ_RA_MAIN");
-            DataGroup.Attribute decColAttr = dg.getAttribute("POS_EQ_DEC_MAIN");
-            if (raColAttr != null && decColAttr != null) {
-                TableMeta.LonLatColumns llc = new TableMeta.LonLatColumns((String)raColAttr.getValue(), (String)decColAttr.getValue(), CoordinateSys.EQ_J2000);
-                dg.addAttributes(new DataGroup.Attribute(MetaConst.CENTER_COLUMN, llc.toString()));
-            }
-            if (dg.getDataDefinitions().length > 0) {
-                String name, desc;
-                for (DataType col : dg.getDataDefinitions()) {
-                    name = col.getKeyName();
-                    desc = col.getShortDesc();
-                    if (desc != null) {
-                        dg.addAttributes(new DataGroup.Attribute(makeAttribKey(DESC_TAG, name.toLowerCase()), desc));
-                    }
-                }
-            }
-
-            File outFile = createFile(req);
-            IpacTableWriter.save(outFile, dg);
-            return outFile;
-        } catch (IOException e) {
-            IOException eio = new IOException("query failed - network Error");
-            eio.initCause(e);
-            throw eio;
-        } catch (EndUserException e) {
-            DataAccessException eio = new DataAccessException("query failed - network error");
-            eio.initCause(e);
-            throw eio;
-        } catch (Exception e) {
-            DataAccessException eio = new DataAccessException("query failed - " + e.toString());
-            eio.initCause(e);
-            throw eio;
-        }
-
-    }
-
-    private File getSearchResult(String urlQuery, String filePrefix) throws IOException, DataAccessException, EndUserException {
-
-        URL url;
-        try {
-            url = new URL(urlQuery);
-        } catch (MalformedURLException e) {
-            _log.error(e, e.toString());
-            throw new EndUserException("query failed - bad url: "+urlQuery, e.toString());
-        }
-        StringKey cacheKey = new StringKey(url);
-        File f = (File) getCache().get(cacheKey);
-        if (f != null && f.canRead()) {
-            return f;
-        } else {
-            URLConnection conn = null;
-
-            //File outFile = createFile(req, ".xml");
-            File outFile = File.createTempFile(filePrefix, ".xml", ServerContext.getPermWorkDir());
-            try {
-                conn = URLDownload.makeConnection(url);
-                conn.setRequestProperty("Accept", "*/*");
-
-                URLDownload.getDataToFile(conn, outFile);
-                getCache().put(cacheKey, outFile, 60 * 60 * 24);    // 1 day
-
-            } catch (MalformedURLException e) {
-                _log.error(e, "Bad URL");
-                throw makeException(e, "query failed - bad url.");
-
-            } catch (IOException e) {
-                _log.error(e, e.toString());
-                if (conn != null && conn instanceof HttpURLConnection) {
-                    HttpURLConnection httpConn = (HttpURLConnection) conn;
-                    int respCode = httpConn.getResponseCode();
-                    if (respCode == 400 || respCode == 404 || respCode == 500) {
-                        InputStream is = httpConn.getErrorStream();
-                        if (is != null) {
-                            String msg = parseMessageFromServer(DynServerUtils.convertStreamToString(is));
-                            throw new EndUserException("query failed: " + msg, msg);
-
-                        } else {
-                            String msg = httpConn.getResponseMessage();
-                            throw new EndUserException("query failed: " + msg, msg);
-                        }
-                    }
-
-                } else {
-                    throw makeException(e, "query failed - network error.");
-                }
-
-            } catch (Exception e) {
-                throw makeException(e, "query failed");
-            }
-            return outFile;
-        }
-
-    }
-
-
-    private String parseMessageFromServer(String response) {
-        // no html, so just return
-        return response.replaceAll("<br ?/?>", "");
-    }
-
-       /*
-        * In the case of error, the service MUST respond with a VOTable
-        * that contains a single PARAM element or a single INFO element
-        * with name="Error", where the corresponding value attribute
-        * should contain some explanation of the nature of the error.
-        * If an INFO element is used, it must appear as a direct child
-        * of one of the following elements:
-        * the root VOTABLE element (which is preferred by this document),
-        * or the RESOURCE element
-        * If a PARAM element is used, it must appear as a direct child
-        * of one of following elements:
-        * the RESOURCE element, or
-        * a DEFINITION element below the root VOTABLE element (which is discouraged by this document).
-        */
-
 }
 /*
 * THIS SOFTWARE AND ANY RELATED MATERIALS WERE CREATED BY THE CALIFORNIA
