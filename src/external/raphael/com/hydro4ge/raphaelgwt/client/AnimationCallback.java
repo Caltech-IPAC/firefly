ff5d2a8cf9273de2132851b3af739d8c3dc79025
roby <roby@ipac.caltech.edu>
2015-03-23 10:54:01 -0700

    remove raphael
    Former-commit-id: 8f227a43d7598cff7321f716e90f6fe295a17fbf
    Former-commit-id: 765240def6cef6419f7f86693b21bbaaf45df693

--- src/external/raphael/com/hydro4ge/raphaelgwt/client/AnimationCallback.java
+++
@@ -1,9 +0,0 @@
-package com.hydro4ge.raphaelgwt.client;
-
-public abstract class AnimationCallback {
-  public abstract void onComplete();
-  static public void fire(AnimationCallback c) {
-	c.onComplete();
-  }
-}
-
