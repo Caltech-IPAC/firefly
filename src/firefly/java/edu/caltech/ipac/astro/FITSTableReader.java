ee4bc3eed6944354072c66e732e7d96370ef456d
ymei <ymei@ipac.caltech.edu>
2015-07-14 11:21:05 -0700

    Modified the old method convertFITSToDataGroup: erased the old logic; added return convertFitsToDataGroup(fits_file, null, null, "TOP_MOST") and deprecated it (the input catName is not used; the data group title is set in the new methods.)

--- src/firefly/java/edu/caltech/ipac/astro/FITSTableReader.java
+++ src/firefly/java/edu/caltech/ipac/astro/FITSTableReader.java
@@ -6,28 +6,21 @@ package edu.caltech.ipac.astro;
 import edu.caltech.ipac.util.DataGroup;
 import edu.caltech.ipac.util.DataObject;
 import edu.caltech.ipac.util.DataType;
-import nom.tam.fits.BasicHDU;
-import nom.tam.fits.BinaryTableHDU;
-import nom.tam.fits.Fits;
 import nom.tam.fits.FitsException;
 import uk.ac.starlink.table.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
-* Convert an Ipac table file to a FITS binary table file 
+* Convert an FITS file or FITS binary table(s) to list of DataGroup.
 */
 public final class FITSTableReader
 {
     public static boolean debug = true;
-
-    private static final int TABLE_INT = 1;
-    private static final int TABLE_DOUBLE = 2;
-    private static final int TABLE_FLOAT = 3;
-    private static final int TABLE_STRING = 4;
-    private static final int TABLE_SHORT = 5;
 
     /**
      * Declare the strategies to handle the FITS data:
@@ -67,7 +60,6 @@ public final class FITSTableReader
 
 
         FITSTableReader fits_to_ipac = new FITSTableReader();
-
 
         //for lsst:
         //String[] dataCols = {"flags", "id", "coord ra", "coord_dec", "base_ClassificationExtendedness_value", "base_SdssCentroid_xSigma"};
@@ -136,20 +128,17 @@ public final class FITSTableReader
 
         try
         {
-            List<DataGroup> dgList = fits_to_ipac.convertFITSToDataGroup(fits_filename, null);
+            //List<DataGroup> dgList = fits_to_ipac.convertFITSToDataGroup(fits_filename, null);
+            List<DataGroup> dgList = fits_to_ipac.convertFitsToDataGroup(fits_filename, null, null, "TOP_MOST");
 
             File output_file = new File(ipac_filename);
-            DataGroup dg = dgList.get(1);
+            DataGroup dg = dgList.get(0);
             IpacTableWriter.save(output_file, dg);
         }
         catch (FitsException fe)
         {
             System.out.println("got FitsException: " + fe.getMessage());
             fe.printStackTrace();
-        }
-        catch (IpacTableException ite)
-        {
-            System.out.println(ite.getMessage());
         }
         catch (IOException ioe)
         {
@@ -158,6 +147,8 @@ public final class FITSTableReader
         }
 
     }
+
+
 
     /**
      * Convert a FITS file to a list of DataGroup.
@@ -278,7 +269,6 @@ public final class FITSTableReader
              * returning only the first datum of the field, even if repeat count is more than 1.
              * This should produce exactly one DataGroup. This is the default strategy if not given.
              */
-
             int repeat = 1;
 
             // One data group per table:
@@ -333,7 +323,7 @@ public final class FITSTableReader
                     List attArrayListTotal = new ArrayList();
                     for (long row = 0; row < nRows; row ++) {
                         Object cell = table.getCell(row, col);
-                        List attArrayList; //???
+                        List attArrayList;
                         attArrayList = getAttArrayList(cell, colInfo);
                         attArrayListTotal.add(attArrayList.get(0));
                     }
@@ -342,7 +332,6 @@ public final class FITSTableReader
                 }
             }
             dataGroupList.add(dataGroup);
-
 
         } else if (strategy.equals(FULLY_FLATTEN)) {
             /**
@@ -536,7 +525,9 @@ public final class FITSTableReader
     }
 
     /**
-     * Collect the cell data to dataArrayList: If the dataArrayList has content, keep adding the cell data to it. If the dataArrayList is empty, make a new one and add the cell data to it.
+     * Collect the cell data to dataArrayList:
+     *      If the dataArrayList has content, keep adding the cell data to it.
+     *      If the dataArrayList is empty, make a new one and add the cell data to it.
      * If the cell is an array, the classType should be primitive. Cast the cell to a primitive data array, data[], based on the class type of the cell.
      * If the cell is a single value, the classType should be the reference type, like Integer. Cast the cell into a reference type based on the class type and put it in the data[].
      * Convert data[repeat] to dataOut[maxRepeat] which is declared at the corresponding reference type:
@@ -880,7 +871,6 @@ public final class FITSTableReader
     private static String getAttributeValue(List attArrayList) throws IOException {
 
         String value = "";
-        //for (int repeat = 0; repeat < cell.length; repeat ++){
         int maxRepeat = attArrayList.size()< maxNumAttriValues? attArrayList.size() : maxNumAttriValues;
         for (int repeat = 0; repeat < maxRepeat; repeat ++) {
             value = value.concat(String.valueOf(attArrayList.get(repeat)));
@@ -914,217 +904,47 @@ public final class FITSTableReader
 
         DataType dataType = new DataType(colName, null);
         Class java_class = null;
-        //String primitive_type = null;
 
         if ((classType.contains("boolean")) || (classType.contains("Boolean"))) {
             if (originalType.contains("L")) {
                 //Logical:
                 java_class = String.class;
-                //primitive_type = "char";
             } else if (originalType.contains("X")) {
                 //Bits:
                 java_class = Integer.class;
-                //primitive_type = "int";
             }
         } else if ((classType.contains("byte")) || (classType.contains("Byte"))) {
             java_class = Integer.class;
-            //primitive_type = "int";
         } else if ((classType.contains("short")) || (classType.contains("Short"))) {
             java_class = Integer.class;
-            //primitive_type = "int";
         } else if ((classType.contains("int")) || (classType.contains("Integer"))) {
             java_class = Integer.class;
-            //primitive_type = "int";
         } else if ((classType.contains("long")) || (classType.contains("Long"))) {
             java_class = Long.class;
-            //primitive_type = "long";
         } else if ((classType.contains("float")) || (classType.contains("Float"))) {
             java_class = Float.class;
-            //primitive_type = "float";
         } else if ((classType.contains("double")) || (classType.contains("Double"))) {
             java_class = Double.class;
-            //primitive_type = "double";
         } else if ((classType.contains("char")) || (classType.contains("String"))) {
             java_class = String.class;
-            //primitive_type = "char";
         } else {
             throw new FitsException(
                     "Unrecognized format character in FITS table file: " + classType);
         }
         dataType.setDataType(java_class);
-        //dataType.setTypeDesc(primitive_type);
         dataType.setUnits(unit);
         return dataType;
     }
 
-
     /**
     * Convert a FITS binary table file on disk to a list of DataGroup
     * @param FITS_filename input_filename
-    * @param catName data group title
+    * @param catName data group title (catName is not used).
+    * @deprecated
     */
-    public static List<DataGroup> convertFITSToDataGroup(String FITS_filename,
-	String catName)
-	throws FitsException, IOException, IpacTableException
+    public static List<DataGroup> convertFITSToDataGroup(String FITS_filename, String catName)
+	throws FitsException, IOException
     {
-
-	DataGroup   _dataGroup;
-	List<DataGroup> _dataGroupList = new ArrayList();
-
-	Fits FITS_file = new Fits(FITS_filename);
-	while (true)
-	{
-	BasicHDU current_hdu = FITS_file.readHDU();
-	if (current_hdu == null)
-	{
-	    break;
-	}
-	if (current_hdu instanceof BinaryTableHDU)
-	{
-	    BinaryTableHDU bhdu = (BinaryTableHDU) current_hdu;
-
-	    int nrows = bhdu.getNRows();
-	    //System.out.println("nrows = " + nrows);
-	    int ncolumns = bhdu.getNCols();
-	    //System.out.println("getNCols() = " + ncolumns);
-	    int format[] = new int[ncolumns];
-	    List<DataType> extraDataList = new ArrayList(ncolumns);
-	    DataType extraData[];
-	    DataType dataType;
-	    String primitive_type;
-	    Class java_class;
-	    int width = 0;
-	    for (int i = 0; i < ncolumns; i++)
-	    {
-		String table_column_format = bhdu.getColumnFormat(i);
-		String table_column_name = bhdu.getColumnName(i);
-		DataType.FormatInfo.Align data_align;
-		//System.out.println("Column " + i + ":  format = " +
-		//    table_column_format + " Name = " + table_column_name); 
-		if (table_column_format.contains("A"))
-		{
-		    format[i] = TABLE_STRING;
-		    java_class = String.class;
-		    primitive_type = "char";
-		    String string_width = table_column_format.substring(
-			 0, table_column_format.indexOf('A'));
-		    //System.out.println("width string = [" + string_width + "]");
-		    width = Integer.valueOf(string_width);
-		    data_align = DataType.FormatInfo.Align.LEFT;
-
-		}
-		else if (table_column_format.contains("J"))
-		{
-		    format[i] = TABLE_INT;
-		    java_class = Integer.class;
-		    primitive_type = "int";
-		    width = 10;
-		    data_align = DataType.FormatInfo.Align.RIGHT;
-		}
-		else if (table_column_format.contains("I"))
-		{
-		    format[i] = TABLE_SHORT;
-		    java_class = Short.class;
-		    primitive_type = "short";
-		    width = 10;
-		    data_align = DataType.FormatInfo.Align.RIGHT;
-		}
-		else if (table_column_format.contains("D")) 
-		{
-		    format[i] = TABLE_DOUBLE;
-		    java_class = Double.class;
-		    primitive_type = "double";
-		    width = 10;
-		    data_align = DataType.FormatInfo.Align.RIGHT;
-		}
-		else if (table_column_format.contains("E"))
-		{
-		    format[i] = TABLE_FLOAT;
-		    java_class = Float.class;
-		    primitive_type = "float";
-		    width = 10;
-		    data_align = DataType.FormatInfo.Align.RIGHT;
-		}
-		else
-		{
-		    throw new FitsException(
-		    "Unrecognized format character in FITS table file: " + 
-		    format[i]);
-		}
-		if (width < table_column_name.length())
-		{
-		    width = table_column_name.length();
-		}
-		dataType= new DataType(table_column_name, java_class);
-		dataType.setTypeDesc(primitive_type);
-		DataType.FormatInfo fi = new DataType.FormatInfo(width + 1);
-		fi.setDataAlign(data_align);
-		//String format_string = "%s";
-		//fi.setDataFormat(format_string);
-		dataType.setFormatInfo(fi);
-		extraDataList.add(dataType);
-	    }
-	    extraData = extraDataList.toArray(new DataType[extraDataList.size()]);
-	    _dataGroup= new DataGroup(catName, extraData);
-
-	    int int_value;
-	    double double_value;
-	    float float_value;
-	    String string_value;
-	    short short_value;
-	    DataObject dataObj = null;
-	    for (int row_number = 0; row_number < nrows; row_number++)
-	    {
-		//System.out.println("Starting row " + row_number );
-		Object[] row = bhdu.getRow(row_number);
-		dataObj = new DataObject(_dataGroup);
-	    for (int i = 0; i < row.length; i++)
-	    {
-		//System.out.print("Starting column " + i + ":  " );
-
-		switch(format[i])
-		{
-		    case TABLE_INT:
-			int int_array[] = (int[])(row[i]);
-
-			int_value = int_array[0];
-			//System.out.println("got int value : " + int_value);
-			Integer java_int = new Integer(int_value);
-			dataObj.setDataElement(extraData[i], java_int);
-			break;
-		    case TABLE_SHORT:
-			short short_array[] = (short[])(row[i]);
-			short_value = short_array[0];
-			//System.out.println("got short value : " + short_value);
-			Short java_short = new Short(short_value);
-			dataObj.setDataElement(extraData[i], java_short);
-			break;
-		    case TABLE_STRING:
-			string_value = (String)row[i];
-			//System.out.println("got string value : " + string_value);
-			dataObj.setDataElement(extraData[i], string_value);
-			break;
-		    case TABLE_DOUBLE:
-			double double_array[] = (double[])(row[i]);
-			double_value = double_array[0];
-			//System.out.println("got double value : "+ double_value);
-			Double v = new Double(double_value);
-			dataObj.setDataElement(extraData[i], v);
-			break;
-		    case TABLE_FLOAT:
-			float float_array[] = (float[])(row[i]);
-			float_value = float_array[0];
-			//System.out.println("got float value : "+ float_value);
-			Float vfloat = new Float(float_value);
-			dataObj.setDataElement(extraData[i], vfloat);
-			break;
-		}
-	    }
-            _dataGroup.add(dataObj);
-	    }
-	    _dataGroupList.add(_dataGroup);
-	}
-	}
-	return _dataGroupList;
+        return convertFitsToDataGroup(FITS_filename, null, null, TOP_MOST);
     }
 }
