c70a3cedf226cb4e5b8d4933f0d7631a1f46837f
roby <roby@ipac.caltech.edu>
2015-10-23 10:59:25 -0700

    missed file

---
+++ src/firefly/js/visualize/ImagePlotCntlr.js
@@ -0,0 +1,37 @@
+
+
+
+import {flux} from '../Firefly.js';
+
+
+
+const ANY_CHANGE= 'ImagePlotCntlr/AnyChange';
+const IMAGE_PLOT_KEY= 'allPlots';
+
+
+
+function reducer(state={}, action={}) {
+    return state;
+}
+
+/*globals ffgwt*/
+
+if (ffgwt) {
+    var allPlots= ffgwt.Visualize.AllPlots.getInstance();
+    allPlots.addListener({
+        eventNotify() {
+            flux.process({type: ANY_CHANGE, payload: {} });
+        }
+    });
+}
+
+
+
+//============ EXPORTS ===========
+//============ EXPORTS ===========
+
+var ImagePlotCntlr = {reducer, ANY_CHANGE, IMAGE_PLOT_KEY };
+export default ImagePlotCntlr;
+
+//============ EXPORTS ===========
+//============ EXPORTS ===========
