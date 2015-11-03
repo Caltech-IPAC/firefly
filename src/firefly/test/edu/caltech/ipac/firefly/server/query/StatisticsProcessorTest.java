f133f9f963ab4d21092e3aa073ae874f88be0af4
Lijun Zhang <zhang@bolo.ipac.caltech.edu>
2015-11-03 10:21:16 -0800

    finalized unit test classes.  Files modified: HistogramProcessor.java, HistogramProcessorTest.java, StatisticsProcessorTest.java

--- src/firefly/test/edu/caltech/ipac/firefly/server/query/StatisticsProcessorTest.java
+++ src/firefly/test/edu/caltech/ipac/firefly/server/query/StatisticsProcessorTest.java
@@ -1,102 +1,100 @@
 package edu.caltech.ipac.firefly.server.query;
+import com.google.gwt.thirdparty.guava.common.annotations.VisibleForTesting;
+import edu.caltech.ipac.astro.IpacTableException;
+import edu.caltech.ipac.astro.IpacTableReader;
+import edu.caltech.ipac.firefly.server.query.StatisticsProcessor;
+import edu.caltech.ipac.util.DataGroup;
+import edu.caltech.ipac.util.DataObject;
+import edu.caltech.ipac.util.*;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import edu.caltech.ipac.astro.IpacTableException;
-import edu.caltech.ipac.astro.IpacTableReader;
-import edu.caltech.ipac.util.DataGroup;
-import edu.caltech.ipac.util.DataObject;
-import edu.caltech.ipac.util.DataType;
-
 /**
  * Created by zhang on 10/29/15.
  */
 public class StatisticsProcessorTest {
-	private static final String TEST_ROOT = "test"+File.separatorChar;
-	private static String filename = StatisticsProcessorTest.class.getCanonicalName().replaceAll("\\.", "/")
-			.replace(StatisticsProcessorTest.class.getSimpleName(), "") + File.separatorChar + "ipacTableTestFile.tbl";
-	private static StatisticsProcessor sp;
-	private static DataGroup inDg;
-	private DataGroup outDg;
-	static File inFile;
 
-	@BeforeClass
-	public static void setUpOnce() throws IpacTableException, IOException, DataAccessException {
+     private static final String TEST_ROOT = "test"+File.separatorChar;
+     private static String filename = StatisticsProcessorTest.class.getCanonicalName().replaceAll("\\.", "/")
+                                                 .replace(StatisticsProcessorTest.class.getSimpleName(), "") + File.separatorChar + "ipacTableTestFile.tbl";
+    private static StatisticsProcessor sp;
+    private static DataGroup inDg;
+    private DataGroup outDg;
+    static File inFile;
 
-		sp = new StatisticsProcessor();
+     @BeforeClass
+    public static void setup() throws IpacTableException, IOException, DataAccessException{
 
-		inFile = new File(TEST_ROOT+filename);
-		inDg = IpacTableReader.readIpacTable(inFile, null, false, "inputTable");
+        sp = new StatisticsProcessor();
 
-	}
+        String fname="/Users/zhang/Work/LSST/TestingData/ipacTableTestFile.tbl";
+        inFile = new File(fname); //filename);
+        inDg = IpacTableReader.readIpacTable(inFile, null, false, "inputTable" );
 
-	@Test
-	public void testCreateStatisticTable() {
 
-		try {
-			outDg = sp.createTableStatistic(inDg);
-			Assert.assertNotNull(outDg);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+    }
 
-	}
+    @Test
+    public void testCreateStatisticTable(){
 
-	@Test
-	public void testStatisticDataArray() {
-		List<DataObject> objList = outDg.values();
-		DataType[] inColumns = outDg.getDataDefinitions();
+        try {
+            outDg = sp.createTableStatistic(inDg);
+            Assert.assertNotNull(outDg);
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
 
-		// Validate the values for each column
-		int nRow = objList.size();
-		String[] colNames = new String[nRow];
-		String[] unitNames = new String[nRow];
-		double[] min = new double[nRow];
-		double[] max = new double[nRow];
-		int[] numPoints = new int[nRow];
-		Object obj;
-		for (int i = 0; i < nRow; i++) {
-			obj = objList.get(i).getDataElement(inColumns[0]);
-			colNames[i] = (String) obj;
-			obj = objList.get(i).getDataElement(inColumns[2]);
-			unitNames[i] = (String) obj;
-			obj = objList.get(i).getDataElement(inColumns[3]);
-			min[i] = ((Double) obj).doubleValue();
-			obj = objList.get(i).getDataElement(inColumns[2]);
-			max[i] = ((Double) obj).doubleValue();
-			obj = objList.get(i).getDataElement(inColumns[2]);
-			numPoints[i] = ((Integer) obj).intValue();
+    }
 
-		}
+    @Test
+    public void testStatisticDataArray(){
+        List<DataObject> objList= outDg.values();
+        DataType[] inColumns = outDg.getDataDefinitions();
 
-		// check to see if all the out DataGroup has the right columns
-		Assert.assertEquals(inColumns.length, 6);
-		String[] expectedColumnNames = { "id", "f_x", "f_y", "i_x", "i_y", "peakValue" };
-		String[] expectedUnits = { "", "pixels", "pixels", "pixels", "pixels", "dn" };
-		double[] expectedMin = { 1, 3, 4, 3, 4, 10.1368 };
-		double[] expectedMax = { 19588, 2044, 1485, 2044, 1485, 9833.3799 };
-		int[] expectedNumPoints = { 1152, 1152, 1152, 1152, 1152, 1152 };
+        //Validate the values for each column
+        int nRow = objList.size();
+        String[] colNames = new String[nRow];
+        String[] unitNames = new String[nRow];
+        double[] min=new double[nRow];
+        double[] max = new double[nRow];
+        int[] numPoints = new int[nRow];
+        Object obj;
+        for (int i =0; i<nRow; i++){
+            obj = objList.get(i).getDataElement(inColumns[0]);
+            colNames[i]=(String) obj;
+            obj = objList.get(i).getDataElement(inColumns[2]);
+            unitNames[i]=(String) obj;
+            obj = objList.get(i).getDataElement(inColumns[3]);
+            min[i]=((Double) obj).doubleValue();
+            obj = objList.get(i).getDataElement(inColumns[4]);
+            max[i]= ((Double) obj).doubleValue();
+            obj = objList.get(i).getDataElement(inColumns[5]);
+            numPoints[i]=((Integer) obj).intValue();
 
-		Assert.assertArrayEquals(expectedColumnNames, colNames);
-		Assert.assertArrayEquals(expectedUnits, unitNames);
+        }
 
-		Assert.assertArrayEquals(expectedMin, min, 10e-10);
-		Assert.assertArrayEquals(expectedMax, max, 10e-10);
-		Assert.assertArrayEquals(expectedNumPoints, numPoints);
+        //check to see if all the out DataGroup has the right columns
+        Assert.assertEquals(inColumns.length, 6);
+        String[] expectedColumnNames={"id", "f_x", "f_y", "i_x", "i_y", "peakValue"};
+        String[] expectedUnits={ "", "pixels", "pixels", "pixels", "pixels", "dn"};
+        double[] expectedMin ={1, 3, 4, 3, 4, 10.1368};
+        double[] expectedMax ={19588, 2044, 1485,  2044, 1485, 9833.3799};
+        int[] expectedNumPoints ={1152, 1152,1152, 1152, 1152, 1152};
 
-	}
+        Assert.assertArrayEquals(expectedColumnNames, colNames);
+        Assert.assertArrayEquals(expectedUnits, unitNames );
 
-	public static void main(String args[]) throws IpacTableException, IOException, DataAccessException {
-		StatisticsProcessorTest myTest = new StatisticsProcessorTest();
-		setUpOnce();
-		myTest.testCreateStatisticTable();
-		myTest.testStatisticDataArray();
+        Assert.assertArrayEquals(expectedMin, min, 10e-10);
+        Assert.assertArrayEquals(expectedMax, max, 10e-10);
+        Assert.assertArrayEquals(expectedNumPoints, numPoints);
 
-	}
+
+    }
+
 }
