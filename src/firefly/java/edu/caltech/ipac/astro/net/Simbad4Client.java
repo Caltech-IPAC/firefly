8fc05f195a066f2dff2f780148b957438bad5dd9
loi <loi@ipac.caltech.edu>
2015-12-09 10:46:43 -0800

    TT #9098: Simbad webservice need to be replaced

--- src/firefly/java/edu/caltech/ipac/astro/net/Simbad4Client.java
+++ src/firefly/java/edu/caltech/ipac/astro/net/Simbad4Client.java
@@ -3,119 +3,95 @@
  */
 package edu.caltech.ipac.astro.net;
 
-import cds.simbad.uif.WSQueryInterface;
-import cds.simbad.uif.WSQueryInterfaceServiceLocator;
-import edu.caltech.ipac.util.download.NetworkManager;
-import edu.caltech.ipac.util.download.HostPort;
 import edu.caltech.ipac.util.ClientLog;
+import edu.caltech.ipac.util.StringUtils;
+import edu.caltech.ipac.util.download.FailedRequestException;
+import edu.caltech.ipac.util.download.URLDownload;
 
-import javax.xml.rpc.ServiceException;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
-import java.io.IOException;
 
 /**
- * This class provide the ability to query Simbad4 Database using web services.
- * Use <tt>searchByName</tt> to query by identifier.<p>
+ * This class provide the ability to query Simbad using Sesame HTTP-GET access.
+ * For more information: http://cdsweb.u-strasbg.fr/doc/sesame.htx
  *
+ *  Below is a sample query output
+ * <p/>
+ * # hip123	#Q-00001
+ * #=Sc=Simbad (CDS, via client/server):    1   105ms
+ * %@ @239810
+ * %C.0 *
+ * %J 0.39989237 +72.23661432 = 00 01 35.974  +72 14 11.81
+ * %J.E [3.34 2.73 90] A 2007A&A...474..653V
+ * %P -43.84 3.28 [0.38 0.31 0] A 2007A&A...474..653V
+ * %V v -13.32 A [0.22] 2005A&A...430..165F
+ * %X 5.91 [0.42] A 2007A&A...474..653V
+ * %S K0 D ~                   =0.0001F400.0000.0000000000000000
+ * %M.B 8.37 [0.02]  D 2000A&A...355L..27H
+ * %M.V 7.21 [0.01]  D 2000A&A...355L..27H
+ * %M.J 5.224 [0.020]  C 2003yCat.2246....0C
+ * %M.H 4.789 [0.034]  C 2003yCat.2246....0C
+ * %M.K 4.613 [0.057]  C 2003yCat.2246....0C
+ * %I.0 HD 224891
+ * #B 8
+ * <p/>
+ * #====Done (2015-Dec-07,22:54:16z)====
  */
 public class Simbad4Client {
 
-    private static final String ENDPOINT_ADDR = "http://%s/axis/services/WSQuery";
-    private static final String QUERY_STRING;
+    public static final String SIMBAD_URL = "http://cdsweb.u-strasbg.fr/cgi-bin/nph-sesame/-oF/S?%s";
 
-    private static final String SEPARATOR = ":";
-
-    /**
-     * Define the query string for each attribute.
-     * When possible, define it in a way so that only the value of
-     * that attribute is returned.  If Simbad returns values separated by
-     * '~', only the first portion of that value will be stored.
-     * The return values will be parsed into a <tt>Map</tt>, keyed by
-     * Atrib.name.
-     */
-    private enum Attrib {
-        RA  ("%.8COO(d;A;ICRS;J2000)"),
-        DEC ("%.8COO(d;D;ICRS;J2000)"),
-        PM_RA  ("%PM(A)"),
-        PM_DEC ("%PM(D)"),
-        PARALLAX ("%PLX(V)"),
-        RV_TYPE ("%RV(T)"),
-        RV_VALUE ("%RV(R)"),
-        SPEC_TYPE ("%SP"),
-        MORPH_TYPE ("%MT"),
-        TYPE ("%OTYPE(V)"),
-        B_MAG  ("%FLUXLIST(B)[%*(F)]"),
-        V_MAG  ("%FLUXLIST(V)[%*(F)]")
-        ;
-        private String _queryString;
-        Attrib(String queryString) {
-            _queryString = queryString;
-        }
-
-        public String toString() {
-            return name() + SEPARATOR + _queryString;
-        }
-
-    }
-
-
-    static {
-        StringBuffer sb = new StringBuffer("");
-        for(Attrib a : Attrib.values()) {
-            sb.append(a).append("\\n");
-        }
-        QUERY_STRING = sb.toString();
-
-    }
-
+    private static final String J2000 = "%J";
+    private static final String PM = "%P";
+    private static final String PLX_V = "%X";
+    private static final String RV = "%V";
+    private static final String SPEC_TYPE = "%S";
+    private static final String MORPH_TYPE = "%T";
+    private static final String TYPE = "%C.0";
+    private static final String B_MAG = "%M.B";
+    private static final String V_MAG = "%M.V";
 
     public SimbadObject searchByName(String objectName)
-                        throws SimbadException, IOException {
+            throws SimbadException, IOException {
 
-        HostPort server = NetworkManager.getInstance().getServer(NetworkManager.SIMBAD4_NAME_RESOLVER);
-
-        ClientLog.message(server.toString());
-        ClientLog.message("Searching for " + objectName);
-
-        WSQueryInterfaceServiceLocator locator = new WSQueryInterfaceServiceLocator();
-        String endPointAddr = String.format(ENDPOINT_ADDR, server.getHost());
-        locator.setWSQueryEndpointAddress(endPointAddr);
+        String url = String.format(SIMBAD_URL, URLEncoder.encode(objectName, "UTF-8"));
         try {
-            WSQueryInterface lookup = locator.getWSQuery();
-            String s = lookup.queryObjectById(objectName,  QUERY_STRING , "txt");
+            String str = URLDownload.getStringFromURL(new URL(url), null);
+            Map<String, String> results = parseResults(str);
 
-            if (s == null || s.length() == 0) {
+
+            if (results.size() == 0) {
                 throw new SimbadException("Simbad did not find the object: " + objectName);
             }
 
-            Map<String,String> attribs = parseResults(s);
 
             SimbadObject sobj = new SimbadObject();
             sobj.setName(objectName);
-            sobj.setType(attribs.get(Attrib.TYPE.name()));
-            sobj.setRa(parseDouble(attribs.get(Attrib.RA.name())));
-            sobj.setRaPM((float) parseDouble(attribs.get(Attrib.PM_RA.name())));
-            sobj.setDec(parseDouble(attribs.get(Attrib.DEC.name())));
-            sobj.setDecPM((float) parseDouble(attribs.get(Attrib.PM_DEC.name())));
-            sobj.setParallax(parseDouble(attribs.get(Attrib.PARALLAX.name())));
-            sobj.setMorphology(attribs.get(Attrib.MORPH_TYPE.name()));
-            sobj.setBMagnitude(parseDouble(attribs.get(Attrib.B_MAG.name())));
+            sobj.setType(getStringVal(results.get(TYPE), " ", 0));
+            sobj.setRa(getDoubleVal(results.get(J2000), " ", 0));
+            sobj.setDec(getDoubleVal(results.get(J2000), " ", 1));
+            sobj.setRaPM((float) getDoubleVal(results.get(PM), " ", 0));
+            sobj.setDecPM((float) getDoubleVal(results.get(PM), " ", 1));
+            sobj.setParallax(getDoubleVal(results.get(PLX_V), " ", 0));
+            sobj.setMorphology(getStringVal(results.get(MORPH_TYPE), " ", 0));
+            sobj.setBMagnitude(getDoubleVal(results.get(B_MAG), " ", 0));
 
-            // spec_type returns more than just the spectral type.  return just the first portion.
-            sobj.setSpectralType(getFirstVal(attribs.get(Attrib.SPEC_TYPE.name()), "\\s"));
+            sobj.setSpectralType(getStringVal(results.get(SPEC_TYPE), " ", 0));
 
-            String rvType = attribs.get(Attrib.RV_TYPE.name());
-            if (rvType.length() > 0) {
+            String rvType = getStringVal(results.get(RV), " ", 0);
+            if (!StringUtils.isEmpty(rvType)) {
                 if (rvType.equals("v")) {
-                    sobj.setRadialVelocity(parseDouble(attribs.get(Attrib.RV_VALUE.name())));
+                    sobj.setRadialVelocity(getDoubleVal(results.get(RV), " ", 1));
                 } else {
-                    sobj.setRedshift(parseDouble(attribs.get(Attrib.RV_VALUE.name())));
+                    sobj.setRedshift(getDoubleVal(results.get(RV), " ", 1));
                 }
             }
 
             // based on previous implementation
-            double vmag = parseDouble(attribs.get(Attrib.V_MAG.name()));
+            double vmag = getDoubleVal(results.get(V_MAG), " ", 0);
             if (Double.isNaN(vmag)) {
                 sobj.setMagBand("B");
                 sobj.setMagnitude(sobj.getBMagnitude());
@@ -126,49 +102,59 @@ public class Simbad4Client {
 
             ClientLog.message("results: " + sobj);
             return sobj;
-        } catch (ServiceException e) {
-            throw new IOException("ServiceException: " + e.getMessage());
         } catch (java.rmi.RemoteException e) {
             throw new SimbadException("Simbad did not find the object: " + objectName);
-            //throw new IOException("RemoteException: " + e.getMessage());
+        } catch (FailedRequestException e) {
+            throw new SimbadException("Simbad did not find the object: " + objectName);
         }
     }
 
-    private String getFirstVal(String s, String sep) {
-        if ( s == null || s.length() == 0) {
+    private String getStringVal(String s, String sep, int pos) {
+        if (StringUtils.isEmpty(s)) {
             return "";
         }
 
-        String[] vals = s.split(sep);
-        return (vals.length > 0) ? vals[0].trim() : "";
+        String[] parts = s.split(sep);
+        return (pos > parts.length) ? "" : parts[pos].trim();
     }
 
-    private Map<String,String> parseResults(String s) {
+    private Map<String, String> parseResults(String s) {
 
         HashMap<String, String> retval = new HashMap<String, String>();
-        for(String v : s.split("\\n") ) {
-            String[] kv = v.split(SEPARATOR, 2);
-            String key = kv[0];
-            String val = kv.length>1 ? kv[1].trim() : "";
-
-            // if the string contains '~', return only the first portion of it.
-            val = getFirstVal(val, "~");
-            retval.put(key, val);
-
+        for (String l : s.split("\\n")) {
+            if (!StringUtils.isEmpty(l) && l.startsWith("%")) {
+                String[] kv = l.split(" ", 2);
+                retval.put(kv[0].trim(), (kv.length > 1 ? kv[1].trim() : null));
+            }
         }
         return retval;
     }
 
-    private double parseDouble(String s) {
-        if(s==null) return Double.NaN;
-
-        try {
-            return Double.parseDouble(s);
-        } catch (NumberFormatException nfx) {
+    private double getDoubleVal(String s, String sep, int pos) {
+        String v = getStringVal(s, sep, pos);
+        if (StringUtils.isEmpty(v)) {
             return Double.NaN;
+        } else {
+            try {
+                return Double.parseDouble(v);
+            } catch (NumberFormatException nfx) {
+                return Double.NaN;
+            }
         }
     }
 
+    public static void main(String[] args) {
+        Simbad4Client client = new Simbad4Client();
+        try {
+            SimbadObject result = client.searchByName("NGC 4321");
+            System.out.println(result.toString());
+        } catch (SimbadException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
 
