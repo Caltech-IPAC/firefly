d5d6e676145a7d2dd2b5b7fff68223694768edbc
Lijun Zhang <zhang@minjia.local>
2016-01-11 21:40:31 -0800

    minor modified the syntax and alignment of the FitsDownloadDialog

--- src/firefly/js/ui/FitsDownloadDialog.jsx
+++ src/firefly/js/ui/FitsDownloadDialog.jsx
@@ -22,287 +22,266 @@ import PlotViewUtil from '../visualize/PlotViewUtil.js';
 import Band from '../visualize/Band.js';
 import {visRoot} from '../visualize/ImagePlotCntlr.js';
 
-//global variable
-//var plotState;
-
 function getDialogBuilder() {
-	var popup= null;
-	return () => {
-		if (!popup) {
-			const popup= (
-					<PopupPanel title={'Fits Download Dialog'} >
-						<FitsDownloadDialog  groupKey={'FITS_DOWNLOAD_FORM'} />
-					</PopupPanel>
-			);
-			DialogRootContainer.defineDialog('fitsDownloadDialog', popup);
-		}
-		return popup;
-	};
+    var popup = null;
+    return () => {
+        if (!popup) {
+            const popup = (
+                <PopupPanel title={'Fits Download Dialog'}>
+                    <FitsDownloadDialog groupKey={'FITS_DOWNLOAD_FORM'}/>
+                </PopupPanel>
+            );
+            DialogRootContainer.defineDialog('fitsDownloadDialog', popup);
+        }
+        return popup;
+    };
 }
 
-
-const fitsDialogBuilder= getDialogBuilder();
+const dialogBuilder = getDialogBuilder();
 
 export function showFitsDownloadDialog() {
-	fitsDialogBuilder();
-	AppDataCntlr.showDialog('fitsDownloadDialog');
+    dialogBuilder();
+    AppDataCntlr.showDialog('fitsDownloadDialog');
 }
 
 
- function getInitialPlotState( ){//plotState) {
+function getInitialPlotState() {
 
-	//plot and plotState are global variable for this class
+    var plot = PlotViewUtil.getActivePlotView(visRoot()).primaryPlot;
 
-	 var plot =  PlotViewUtil.getActivePlotView(visRoot()).primaryPlot;
-	 //var plotState =plot.plotState;
-	 var plotState=plot.plotState;
+    var plotState = plot.plotState;
 
-	var threeColorBandUsed=false;
+    var threeColorBandUsed = false;
 
-	var color;
-		if (plotState.isThreeColor()) {
-		threeColorBandUsed==true;
-		var bands = this.plotState.getBands();
+    var color;
+    if (plotState.isThreeColor()) {
+        threeColorBandUsed == true;
+        var bands = this.plotState.getBands();
 
-		var colorID=bands[0].toString();
-		color = Band.valueOf()[colorID].name();
+        var colorID = bands[0].toString();
+        color = Band.valueOf()[colorID].name();
 
-	}
+    }
 
 
+    var isCrop = plotState.hasOperation(Operation.CROP);
+    var isRotation = plotState.hasOperation(Operation.ROTATE);
+    var cropNotRotate = isCrop && !isRotation ? true : false;
 
-	var isCrop = plotState.hasOperation(Operation.CROP);
-	var isRotation =  plotState.hasOperation(Operation.ROTATE);
-	var cropNotRotate =  isCrop && !isRotation ? true: false;
-
-	return {
-		plotState,
-		color,
-		hasThreeColorBand:threeColorBandUsed,
-		hasOperation: cropNotRotate
-	};
+    return {
+        plotState,
+        color,
+        hasThreeColorBand: threeColorBandUsed,
+        hasOperation: cropNotRotate
+    };
 
 }
 
 
 class FitsDownloadDialog extends React.Component {
 
-	 constructor(props) {
-		super(props);
-		FieldGroupUtils.initFieldGroup('FITS_DOWNLOAD_FORM');
-		this.state = {fields: FieldGroupUtils.getGroupFields('FITS_DOWNLOAD_FORM')};//, plotState:PlotViewUtil.getActivePlotView(visRoot()).primaryPlot.plotState};
+    constructor(props) {
+        super(props);
+        FieldGroupUtils.initFieldGroup('FITS_DOWNLOAD_FORM');
+        this.state = {fields: FieldGroupUtils.getGroupFields('FITS_DOWNLOAD_FORM')};
+
+    }
 
 
-	  }
+    componentWillUnmount() {
+
+        if (this.unbinder) this.unbinder();
+    }
 
 
-		componentWillUnmount()
-		{
-			if (this.unbinder) this.unbinder();
-		}
+    componentDidMount() {
+
+        this.unbinder = FieldGroupUtils.bindToStore('FITS_DOWNLOAD_FORM', (fields) => {
+            this.setState({fields});
+        });
+    }
 
 
-		componentDidMount()
-		{
-			//this.unbinder = FieldGroupUtils.bindToStore('FITS_DOWNLOAD_FORM', (fields, plotState) => {
+    render() {
 
-			this.unbinder= FieldGroupUtils.bindToStore('FITS_DOWNLOAD_FORM', (fields) => {
-				this.setState({fields});
-			});
-		}
+        var {fields}= this.state;
+        if (!fields) return false;
 
-
-render()
-		{
-			//var {fields}= this.state;//.fields;
-			//var {plotState }= this.state.plotState;
-
-			var {fields}= this.state;
-			if (!fields) return false;
-
-			return <FitsDownloadDialogForm  />;//plotSate={plotState} />;
-		}
+        return <FitsDownloadDialogForm  />;
+    }
 
 
 }
 /// Fits dialog test
 
-function renderOperationOption(hasOperation){
+function renderOperationOption(hasOperation) {
 
-	if (hasOperation) {
-		return (
-           <div>
-				<RadioGroupInputField
-					initialState={{
+    if (hasOperation) {
+        return (
+            <div>
+                <RadioGroupInputField
+                    initialState={{
                            tooltip: 'Please select an option',
                            label : 'FITS file:'
                            }}
-					options={[
+                    options={[
                             { label:'original', value:'fileTypeOrig'},
                             { label:'crop', value:'fileTypeCrop'}
 
                             ]}
-					alignment={'vertical'}
-					fieldKey='operationOption'
+                    alignment={'vertical'}
+                    fieldKey='operationOption'
 
-				/>
-	      <br/>
-	 </div>
-		);
-	}
-	else {
-		return <br/>;
-	}
+                />
+                <br/>
+            </div>
+        );
+    }
+    else {
+        return <br/>;
+    }
 }
 
-function renderThreeBand(hasThreeColorBand,  color) {
-	if (hasThreeColorBand) {
-		return (
-				<div>
-				<RadioGroupInputField
-					initialState={{
+function renderThreeBand(hasThreeColorBand, color) {
+    if (hasThreeColorBand) {
+        return (
+            <div>
+                <RadioGroupInputField
+                    initialState={{
                       tooltip: 'Please select an option',
                       label : 'Color band:'
                   }}
-					options={[
+                    options={[
                            {  label: color,
                               value: 'colorOpt'
                            }
                          ]}
-					alignment={'vertical'}
-					fieldKey='threeBandColor'
+                    alignment={'vertical'}
+                    fieldKey='threeBandColor'
 
-				/>
+                />
 
-				<br/>
-				</div>
+                <br/>
+            </div>
 
-		);
-	}
+        );
+    }
     else {
-		return <br/>;
-	}
+        return <br/>;
+    }
 }
 
-function FitsDownloadDialogForm () {
+function FitsDownloadDialogForm() {
 
 
-	const { color, plotState, hasThreeColorBand, hasOperation} = getInitialPlotState();
-
-	//this.plotState=plotState;
+    const { plotState, color, hasThreeColorBand, hasOperation} = getInitialPlotState();
 
     var renderOperationButtons = renderOperationOption(hasOperation);
-	var renderThreeBandButtons = renderThreeBand(hasThreeColorBand, color);
-	return (
-		<FieldGroup groupKey='FITS_DOWNLOAD_FORM'  keepState={true}>
-			<div style={{padding:'5px'}}>
-				<div style={{'minWidth': '300', 'minHeight': '100'} }>
-					<InputGroup labelWidth={130} >
-						<PopupPanel  />
-					    	<RadioGroupInputField
-							initialState= {{
+    var renderThreeBandButtons = renderThreeBand(hasThreeColorBand, color);
+    return (
+        <FieldGroup groupKey='FITS_DOWNLOAD_FORM' keepState={true}>
+            <div style={{ padding:5 }}>
+                <div style={{ minWidth : 300, minHeight: 100 } }>
+                    <InputGroup labelWidth={130}>
+                        <PopupPanel  />
+
+                        <RadioGroupInputField
+
+                            initialState={{
                                     tooltip: 'Please select an option',
                                     label : 'Type of files:'
                                    }}
-							options={ [
+
+                            options={ [
                                       {label: 'FITS File', value: 'fits'},
                                       {label: 'PNG File', value: 'png' },
                                        {label: 'Region File', value: 'reg'}
                                     ]}
-							alignment={'vertical'}
-							fieldKey='fileType'
-					    	/>
-					    	
-						      {renderOperationButtons}
-					    	  {renderThreeBandButtons}
+                            alignment={'vertical'}
+                            fieldKey='fileType'
+                        />
 
-						   <div style={{'text-align':'center'}}>
-							 < CompleteButton groupKey='FITS_DOWNLOAD_FORM'
-								text='Download'
-								onSuccess={(request) => resultsSuccess(request,plotState)}
-								onFail={resultsFail}
-								dialogId='FitsDownloadDialog'
-							 />
-						<br/>
-						</div>
-					</InputGroup>
-				</div>
-			</div>
-		</FieldGroup>
-	);
+                        {renderOperationButtons}
+                        {renderThreeBandButtons}
+
+                        <div style={{'text-align':'center'}}>
+                            < CompleteButton
+                                groupKey='FITS_DOWNLOAD_FORM'
+                                text='Download'
+                                onSuccess={ (request) => resultsSuccess(request, plotState )}
+                                onFail={resultsFail}
+                                dialogId='fitsDownloadDialog'
+                            />
+                            <br/>
+                        </div>
+                    </InputGroup>
+                </div>
+            </div>
+        </FieldGroup>
+    );
 
 }
-
-
-
-FitsDownloadDialogForm.propTypes= {
-	fields: React.PropTypes.object.isRequired
-
-};
 
 
 function showResults(success, request) {
 
-	var rel = {};
-	console.log(request);
+    var rel = {};
+    console.log(request);
 
-	if (success) {
-
-
-	   Object.keys(request).reduce(function (buildString, k, idx, array) {
-		 if (idx < array.length - 1)  return;
-		   rel[k] = request[k];
+    if (success) {
 
 
-	   }, '');
+        Object.keys(request).reduce(function (buildString, k, idx, array) {
+            if (idx < array.length - 1)  return;
+            rel[k] = request[k];
+
+        }, '');
     }
-	return rel;
+    return rel;
 }
-
 
 
 function resultsFail(request) {
-	showResults(false,request);
+    showResults(false, request);
 }
 
-function resultsSuccess(request,plotState) {
-	var rel = showResults(true,request);
+function resultsSuccess(request, plotState) {
+    var rel = showResults(true, request);
 
-    if (Object.keys(rel).length==0){
-		console.log(request);
-		return resultsFail(request);
-	}
-	var ext;
-	var bandSelect=null;
-	var whichOp=null;
-	for (var key in rel){
-		var value=rel[key];
-		if (key=='fileType'){
-			ext = value;
+    if (!Object.keys(rel).length) {
+        console.log(request);
+        return resultsFail(request);
+    }
+    var ext;
+    var bandSelect;
+    var whichOp;
+    Object.keys(rel).forEach(function (key) {
+        var value = rel[key];
+        if (key === 'fileType') {
+            ext = value;
+        }
+        if (key === 'threeBandColor') {
+            bandSelect = value;
+        }
+        if (key === 'operationOption') {
+            whichOp = value;
+        }
+    });
 
-		}
-		if (key == 'threeBandColor'){
-			bandSelect=value;
-		}
-		if (key == 'operationOption'){
-			whichOp=value;
-		}
-	}
-
-	var band = Band.NO_BAND;
-	if (bandSelect != null) {
-		band = Band[bandSelect];
-	}
+    var band = Band.NO_BAND;
+    if (bandSelect) {
+        band = Band[bandSelect];
+    }
 
 
-	var fitsFile =plotState.getOriginalFitsFileStr(band) == 'undefined' || whichOp == null ?
-			plotState.getWorkingFitsFileStr(band) :
-			plotState.getOriginalFitsFileStr(band);
+    var fitsFile = !plotState.getOriginalFitsFileStr(band) || !whichOp ?
+        plotState.getWorkingFitsFileStr(band) :
+        plotState.getOriginalFitsFileStr(band);
 
-	if (ext.toLowerCase() =='fits') {
+    if (ext && ext.toLowerCase() == 'fits') {
 
-		download(getRootURL() + '/servlet/Download?file='+ fitsFile);
-	}
+        download(getRootURL() + '/servlet/Download?file=' + fitsFile);
+    }
 
 }
 
