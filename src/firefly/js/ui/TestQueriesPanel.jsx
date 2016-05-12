35b33f1634ee831344fb3337ea55e0668922677d
roby <roby@ipac.caltech.edu>
2016-05-11 18:36:46 -0700

    fixed update issue

--- src/firefly/js/ui/TestQueriesPanel.jsx
+++ src/firefly/js/ui/TestQueriesPanel.jsx
@@ -361,10 +361,10 @@ function doImages(request) {
     wpr2.setPlotGroupId('test-group');
     wpr3.setPlotGroupId('test-group');
     wpr4.setPlotGroupId('test-group');
-    wpr1.setGroupLocked(false);
-    wpr2.setGroupLocked(false);
-    wpr3.setGroupLocked(false);
-    wpr4.setGroupLocked(false);
+    wpr1.setGroupLocked(true);
+    wpr2.setGroupLocked(true);
+    wpr3.setGroupLocked(true);
+    wpr4.setGroupLocked(true);
 
 
 
