ed045980b0afc4fac9b11ab3605dbdfac8db2a8c
roby <roby@ipac.caltech.edu>
2016-07-18 16:21:06 -0700

    fix undefined bug on mouse leave

--- src/firefly/js/visualize/iv/ImageViewerLayout.jsx
+++ src/firefly/js/visualize/iv/ImageViewerLayout.jsx
@@ -287,7 +287,7 @@ function makeMessageArea(pv,plotShowing) {
 function findMouseOwner(dlList, plot, screenPt) {
                     // Step 1
     const exList= dlList.filter((dl) => get(dl,'exclusiveDef.exclusiveOnDown'));
-    if (isEmpty(exList)) return null;
+    if (isEmpty(exList) || ! screenPt) return null;
 
                     // Step 2
     const nowList= exList.filter((dl) => get(dl,'exclusiveDef.type','')==='anywhere');
