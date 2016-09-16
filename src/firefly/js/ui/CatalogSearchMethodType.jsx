e91e88bbdcbe269e7f23de98277b06532ac831df
ejoliet <ejoliet@ipac.caltech.edu>
2016-09-16 16:12:29 -0700

    DM-7487: add polygon values from active image in catalog search

--- src/firefly/js/ui/CatalogSearchMethodType.jsx
+++ src/firefly/js/ui/CatalogSearchMethodType.jsx
@@ -16,6 +16,8 @@ import {SizeInputFields} from './SizeInputField.jsx';
 import {InputAreaFieldConnected} from './InputAreaField.jsx';
 import {FileUpload} from '../ui/FileUpload.jsx';
 
+import {getActiveTarget} from '../core/AppDataCntlr.js';
+
 import './CatalogSearchMethodType.css';
 /*
  Component which suppose to handle the catalog method search such as cone, elliptical,etc.
@@ -44,7 +46,9 @@ export class CatalogSearchMethodType extends Component {
 
     render() {
         const {fields}= this.state;
-        const searchType = get(fields, 'spatial.value', SpatialMethod.Cone.value);
+
+
+        let searchType = get(fields, 'spatial.value', SpatialMethod.Cone.value);
         const max = get(fields, 'conesize.max', 10);
         const {groupKey} = this.props;
 
@@ -182,16 +186,29 @@ function sizeArea(searchType, max) {
             </div>
         );
     } else if (searchType === SpatialMethod.Polygon.value) {
-
+        let val = '';
+        var pv = getActivePlotView(visRoot());
+        if (pv) {
+            var plot = pv.plots[0];
+            var w = plot.dataWidth;
+            var h = plot.dataHeight;
+            var cc = CsysConverter.make(plot);
+            var pt1 = cc.getWorldCoords(makeImagePt(w/4,h/4));
+            var pt2 = cc.getWorldCoords(makeImagePt(3*w/4, h/4));
+            var pt3 = cc.getWorldCoords(makeImagePt(3*w/4, 3*h/4));
+            var pt4 = cc.getWorldCoords(makeImagePt(w/4, 3*h/4));
+            val = `${pt1.x} ${pt1.y}, ${pt2.x} ${pt2.y},${pt3.x} ${pt3.y},${pt4.x} ${pt4.y}`;
+        }
         return (
             <div
                 style={{padding:5, border:'solid #a3aeb9 1px' }}>
-                <InputAreaFieldConnected fieldKey='polygoncoords'
+                <InputAreaFieldConnected forceReinit={true} fieldKey='polygoncoords'
                                          wrapperStyle={{padding:5}}
                                          style={{overflow:'auto',height:'65px', maxHeight:'200px', width:'220px', maxWidth:'300px'}}
                                          initialState={{
                                                tooltip:'Enter polygon coordinates search',
-                                               labelWidth:70
+                                               labelWidth:70,
+                                               value: val
                                             }}
                                          label='Coordinates:'
                                          tooltip='Enter polygon coordinates search'
@@ -253,3 +270,77 @@ var initRadiusArcSec = (max) => {
         return parseFloat(1 / 3600).toString();
     }
 };
+
+
+/**
+ * Helper to get polygon from image
+ */
+import CsysConverter from '../visualize/CsysConverter.js';
+import {getActivePlotView} from '../visualize/PlotViewUtil.js';
+import { makeImagePt} from '../visualize/Point.js';
+import {visRoot} from '../visualize/ImagePlotCntlr.js';
+import {MetaConst} from '../data/MetaConst.js';
+import {CoordinateSys} from '../visualize/CoordSys.js';
+import {doFetchTable, getTblById, getActiveTableId, getColumnIdx, getTableInGroup, isTableUsingRadians} from '../tables/TableUtil.js';
+const DEF_CORNER_COLS= ['ra1;dec1', 'ra2;dec2', 'ra3;dec3', 'ra4;dec4'];
+function getCovColumnsForQuery(table) {
+    const cAry = [...getCornersColumns(table), getCenterColumns(table)];
+    return cAry.reduce((s, c, idx)=> s + `${idx > 0 ? ',' : ''}${c.lonCol},${c.latCol}`, '');
+}
+function getCornersColumns(table) {
+    if (!table) return [];
+    const {tableMeta}= table;
+    if (!tableMeta) return [];
+    if (tableMeta[MetaConst.ALL_CORNERS]) {
+        return makeCoordColAry(tableMeta[MetaConst.ALL_CORNERS].split(','), table);
+    }
+    return makeCoordColAry(DEF_CORNER_COLS, table);
+}
+
+function getCenterColumns(table) {
+    if (!table) return [];
+    const {tableMeta:meta}= table;
+    if (!meta) return [];
+
+    if (meta[MetaConst.CENTER_COLUMN]) return makeCoordCol(meta[MetaConst.CENTER_COLUMN],table);
+    if (meta[MetaConst.CATALOG_COORD_COLS]) return makeCoordCol(meta[MetaConst.CATALOG_COORD_COLS],table);
+    const defCol= guessDefColumns(table);
+
+    return makeCoordCol(defCol,table);
+}
+
+function guessDefColumns(table) {
+    const DEF_CENTER_COL= 'ra;dec;EQ_J2000';
+    const {columns}= table.tableData;
+    const colList= columns.map( (c) => c.name.toLowerCase());
+    if (colList.includes('ra') && colList.includes('dec')) return 'ra;dec;EQ_J2000';
+    if (colList.includes('lon') && colList.includes('lat')) return 'lon;lat;EQ_J2000';
+    // if (colList.includes('crval1') && colList.includes('crval2')) return 'crval1;crval2;EQ_J2000';
+    return 'ra;dec;EQ_J2000';
+}
+
+function getBoxAryFromTable(options, table, usesRadians) {
+    const cDefAry = getCornersColumns(table);
+    return table.tableData.data
+        .map((row) => cDefAry
+            .map((cDef) => makeWorldPt(row[cDef.lonIdx], row[cDef.latIdx], cDef.csys)));
+}
+
+
+const makeCoordColAry = (cAry, table) => cAry.map((c) => makeCoordCol(c, table)).filter((cCol) => cCol);
+
+function makeCoordCol(def, table) {
+    const s = def.split(';');
+    if (s.length !== 3 && s.length !== 2) return null;
+    const s0Idx = getColumnIdx(table, s[0]);
+    const s1Idx = getColumnIdx(table, s[1]);
+    if (s0Idx === -1 || s0Idx === -1) return null;
+    return {
+        lonCol: s[0],
+        latCol: s[1],
+        lonIdx: s0Idx,
+        latIdx: s1Idx,
+        csys: s[2] ? CoordinateSys.parse(s[2]) : CoordinateSys.EQ_J2000
+    };
+
+}
