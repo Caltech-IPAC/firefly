4153d3eb2582b33cf9af64c887ed4087259a8e28
wmi <wmi@ipac.caltech.edu>
2016-09-23 10:29:56 -0700

    Add ibe server request retrieve the images. Fixed  the makeProcessorRequest bug and few eslint errors in WebPLotRequest.

--- src/firefly/js/templates/lightcurve/LcManager.js
+++ src/firefly/js/templates/lightcurve/LcManager.js
@@ -2,21 +2,23 @@
  * License information at https://github.com/Caltech-IPAC/firefly/blob/master/License.txt
  */
 
+import {isUndefined, get} from 'lodash';
 import {take} from 'redux-saga/effects';
-import {get} from 'lodash';
 
 import {LO_VIEW, LO_MODE, SHOW_DROPDOWN, SET_LAYOUT_MODE, getLayouInfo, dispatchUpdateLayoutInfo, dropDownHandler} from '../../core/LayoutCntlr.js';
 import {TBL_RESULTS_ADDED, TABLE_LOADED, TBL_RESULTS_ACTIVE, TABLE_HIGHLIGHT} from '../../tables/TablesCntlr.js';
 import {getCellValue, getTblById, makeTblRequest} from '../../tables/TableUtil.js';
 import {updateSet} from '../../util/WebUtil.js';
 import {dispatchPlotImage} from '../../visualize/ImagePlotCntlr.js';
-import WebPlotRequest from '../../visualize/WebPlotRequest.js';
+import {WebPlotRequest,TitleOptions} from '../../visualize/WebPlotRequest.js';
+import {makeWiseLcPlotRequest} from '../../metaConvert/WiseLcViewRequestList';
+import {converters, converterFactory} from '../../metaConvert/ConverterFactory.js';
+import {ServerRequest} from '../../data/ServerRequest.js';
 
 export const RAW_TABLE = 'raw_table';
 export const PHASE_FOLDED = 'phase_folded';
 export const PERIODOGRAM = 'periodogram';
 export const PEAK_TABLE = 'peak_table';
-
 export const IMG_VIEWER_ID = 'lc_image_viewer';
 
 /**
@@ -94,55 +96,43 @@ function handleTableHighlight(layoutInfo, action) {
 
 function getWebPlotRequest(tbl_id) {
     const tableModel = getTblById(tbl_id);
-    const ra = getCellValue(tableModel, tableModel.highlightedRow, 'ra');
-    const dec = getCellValue(tableModel, tableModel.highlightedRow, 'dec');
-
-    console.log(`${ra} , ${dec}`);
-    //Example: frame-id = "02328b152-w1",
-    // see meaning: http://wise2.ipac.caltech.edu/docs/release/allwise/expsup/sec3_1a.html#frame_id
-
-    const frame_id = getCellValue(tableModel, tableModel.highlightedRow, 'frame_id');
-
-    var wise_sexp_ibe = /(\d+)([0-9][a-z])(\w+)/g;
-
-    var res = wise_sexp_ibe.exec(frame_id);
-    /*
-     will split 'frame_id' into 5 elements array
-     02328b152 - full frame-id
-     0232 - pre-scanid, (+ scangroup =  scan_id)
-     8d - scangrp ,
-     152 - frame_num
-
-     see http://irsa.ipac.caltech.edu/ibe/docs/wise/merge/merge_p1bm_frm/#int
-
-     IBE url:
-     http://irsa.ipac.caltech.edu/ibe/data/wise/merge/merge_p1bm_frm/{scangrp:s}/{scan_id:s}/{frame_num:03d}/{scan_id:s}{frame_num:03d}-w{band:1d}-int-1b.fits
-     */
-
-    const scan_id = res[1]+res[2];
+    const hlrow = tableModel.highlightedRow || 0;
+    const ra = getCellValue(tableModel, hlrow, 'ra');
+    const dec = getCellValue(tableModel, hlrow, 'dec');
+    const frameId = getCellValue(tableModel, hlrow, 'frame_id');
+    var   wise_sexp_ibe = /(\d+)([0-9][a-z])(\w+)/g;
+    var   res = wise_sexp_ibe.exec(frameId);
+    const scan_id = res[1] + res[2];
     const scangrp = res[2];
     const frame_num = res[3];
-    console.log(`http://irsa.ipac.caltech.edu/ibe/data/wise/merge/merge_p1bm_frm/${scangrp}/${scan_id}/${frame_num}/${scan_id}${frame_num}-w1-int-1b.fits`);
-    // return makeWebPlotRequest(tbl_id);  should use the commented code below to retrieve wise images.
-    // this is just a placeholder.
-    //return  {
-    //    plotId: 'lc_images',
-    //    Type     : 'SERVICE',
-    //    Service  : 'WISE',
-    //    Title    : 'Wise',
-    //    SurveyKey  : 'Atlas',
-    //    SurveyKeyBand  : '2',
-    //    WorldPt    : `${ra};${dec};EQ_J2000`,
-    //    SizeInDeg  : '.3',
-    //    AllowImageSelection : true
-    //};
 
-    const url = `http://irsa.ipac.caltech.edu/ibe/data/wise/merge/merge_p1bm_frm/${scangrp}/${scan_id}/${frame_num}/${scan_id}${frame_num}-w1-int-1b.fits`;
-    var wpr = WebPlotRequest.makeURLPlotRequest(url);
-    return wpr;
+    const sr= new ServerRequest('ibe_file_retrieve');
+    sr.setParam('mission', 'wise');
+    sr.setParam('plotId', 'lc_images');
+    sr.setParam('PROC_ID', 'ibe_file_retrieve');
+    sr.setParam('ProductLevel',  '1b');
+    sr.setParam('ImageSet', 'allsky-4band');
+    sr.setParam('band', 2);
+    sr.setParam('scangrp', `${scangrp}`);
+    sr.setParam('scan_id', `${scan_id}`);
+    sr.setParam('frame_num', `${frame_num}`);
+    sr.setParam('center', `${ra},${dec}`);
+    sr.setParam('size', '0.3');
+    sr.setParam('subsize', '0.3');
+    sr.setParam('in_ra',`${ra}`);
+    sr.setParam('in_dec',`${dec}`);
+
+    const reqParams = WebPlotRequest.makeProcessorRequest(sr, 'wise');
+    reqParams.setTitle('WISE-'+ frameId);
+//    reqParams.setInitialZoomLevel(0.5);
+    return reqParams;
+
+
+
 }
 
 
-function makeWebPlotRequest() {
+function makeWebPlotRequest(tbl_id) {
+//todo make grid plotting with hlrow+1, hlrow, hlrow-1
     // for WISE, should convert gwt code from here: edu.caltech.ipac.hydra.server.query.WiseGrid.makeRequest;
 }
