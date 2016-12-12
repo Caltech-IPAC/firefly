e5f1644a3acd617fa1684bd2af4a804adda0fa94
Lijun Zhang <zhang@boai.ipac.caltech.edu>
2016-12-12 10:43:46 -0800

    DM-7779: add unit test for FitsRead class          add FitsValidation class as a base class for FITS related unit test  validation

--- src/firefly/test/edu/caltech/ipac/visualize/plot/CropTest.java
+++ src/firefly/test/edu/caltech/ipac/visualize/plot/CropTest.java
@@ -1,11 +1,10 @@
 package edu.caltech.ipac.visualize.plot;
 
 import edu.caltech.ipac.firefly.util.FileLoader;
+import edu.caltech.ipac.firefly.util.FitsValidation;
 import nom.tam.fits.*;
 import nom.tam.util.BufferedDataOutputStream;
-import nom.tam.util.Cursor;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
 
@@ -20,14 +19,14 @@ import java.io.*;
  *
  * It is difficult to do a unit test for this kind cases.  We the end to end test is used instead.
  */
-public class CropTest {
+public class CropTest extends FitsValidation{
 
 
     private static String fileName = "f3.fits";
     private  Fits  inFits=null;
     private  String resultFitsName="out_f3.fits";
     private  Fits expectedFits =null;
-    private  float delta =0.1f-10;
+
 
     @Before
     /**
@@ -52,45 +51,6 @@ public class CropTest {
         expectedFits=null;
     }
 
-    /**
-     * Validate the header card one by one
-     * @param expectedFits
-     * @param calculatedFits
-     * @throws FitsException
-     */
-    private void validHeader (Fits expectedFits, Fits calculatedFits) throws FitsException {
-        BasicHDU[] expectedHDUs =  expectedFits.read();
-
-        BasicHDU[] outHDUs =calculatedFits.read();
-        for (int i=0; i<expectedHDUs.length; i++){
-            Header expectedHeader = expectedHDUs[i].getHeader();
-            Header calculatedHeader = outHDUs[i].getHeader();
-            Assert.assertEquals(expectedHeader.getNumberOfCards(), calculatedHeader.getNumberOfCards());
-            Cursor  expectedIter = 	expectedHeader.iterator();
-            Cursor  calculatedIter = 	calculatedHeader.iterator();
-            while(expectedIter.hasNext() && calculatedIter.hasNext()){
-                HeaderCard expectedCard = (HeaderCard) expectedIter.next();
-                HeaderCard calculatedCard = (HeaderCard) calculatedIter.next();
-                Assert.assertEquals( expectedCard.getKey(), calculatedCard.getKey());
-                Assert.assertEquals( expectedCard.getValue(), calculatedCard.getValue());
-            }
-        }
-    }
-
-    /**
-     * Validate the data stored in the FITS file
-     * @param expectedFits
-     * @param calculatedFits
-     * @throws FitsException
-     */
-    private void validateData(Fits expectedFits, Fits calculatedFits) throws FitsException {
-        FitsRead[] fitsReads = FitsRead.createFitsReadArray(calculatedFits);
-        FitsRead[] expectedFitsRead = FitsRead.createFitsReadArray(expectedFits);
-        for (int i=0;  i<fitsReads.length; i++){
-            Assert.assertArrayEquals(fitsReads[i].getDataFloat(), expectedFitsRead[i].getDataFloat(), delta);
-        }
-
-    }
     @Test
     /**
      * We use the result wen this class written as a reference.  The output is saved to a file, out_f3.fits.  The newly calculated
@@ -102,9 +62,7 @@ public class CropTest {
 
         Fits  outFits = Crop.do_crop(inFits, min_x, min_y, max_x, max_y);
 
-        validHeader(expectedFits,outFits);
-
-        validateData(expectedFits,outFits);
+        validateFits(expectedFits,outFits);
 
     }
 
