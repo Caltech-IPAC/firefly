0bdc67dbfcf360cf37eebfb10e1189c6d506880f
ejoliet <ejoliet@ipac.caltech.edu>
2017-02-06 17:44:16 -0800

    DM-7195: cleanup search processor of unused innerclass

--- src/firefly/java/edu/caltech/ipac/firefly/server/query/lc/LightCurveFileGroupsProcessor.java
+++ src/firefly/java/edu/caltech/ipac/firefly/server/query/lc/LightCurveFileGroupsProcessor.java
@@ -308,36 +308,5 @@ public class LightCurveFileGroupsProcessor extends FileGroupsProcessor {
 
         return fgArr;
     }
-
-    class CutoutFilenameResolver implements FileInfo.FileNameResolver {
-        private String _cutoutData;
-        private String _pathStructure;
-
-        public CutoutFilenameResolver(String data, String path) {
-            _cutoutData = data;
-            _pathStructure = path;
-        }
-
-        public String getResolvedName(String input) {
-            String fileName;
-            String gzExt = "";
-            String path = StringUtils.isEmpty(_pathStructure) ? "" : _pathStructure;
-
-            if (FileUtil.isExtension(input, FileUtil.GZ)) {
-                gzExt = "." + FileUtil.GZ;
-                input = FileUtil.getBase(input);
-            }
-
-            String base = FileUtil.getBase(input);
-            if (!StringUtils.isEmpty(base)) {
-                fileName = path + base + _cutoutData + "." + FileUtil.getExtension(input) + gzExt;
-            } else {
-                fileName = path + input + _cutoutData + gzExt;
-            }
-
-            fileName = fileName.replaceAll(",", "_");
-            return fileName;
-        }
-    }
 }
 
