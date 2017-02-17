e4c68e5c81cc61b4897aafcc344e8466a6818d68
ejoliet <ejoliet@ipac.caltech.edu>
2017-02-16 18:05:50 -0800

    peaks table SDE column sort order fixed

--- src/firefly/js/templates/lightcurve/LcPeriodogram.jsx
+++ src/firefly/js/templates/lightcurve/LcPeriodogram.jsx
@@ -510,7 +510,7 @@ function periodogramSuccess(popupId, hideDropDown = false) {
             step_size: ssize ? ssize : undefined,
             peaks: get(request, [pKeyDef.peaks.fkey]),
             table_name: LC.PEAK_TABLE,
-            sortInfo: sortInfoString('SDE')                 // sort peak table by column SDE
+            sortInfo: sortInfoString('SDE', false)                 // sort peak table by column SDE, descending
         }, {tbl_id: LC.PEAK_TABLE, pageSize: parseInt(peak)});
 
         if (tReq2 !== null) {
@@ -533,6 +533,8 @@ function periodogramSuccess(popupId, hideDropDown = false) {
             step_size: ssize ? ssize : undefined,
             peaks: get(request, [pKeyDef.peaks.fkey]),
             table_name: LC.PERIODOGRAM_TABLE
+            /* Should we do the same for Power column in Periodogram? */
+            /*sortInfo: sortInfoString('Power', false)*/
         }, {tbl_id: LC.PERIODOGRAM_TABLE});
 
 
