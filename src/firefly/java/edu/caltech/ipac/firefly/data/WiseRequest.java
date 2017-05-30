5928fb80a2c86b87cb7e4245371e25d5c00ed913
wmi <wmi@ipac.caltech.edu>
2017-05-30 13:53:03 -0700

    IRSA-250 Set the neowiser catalog name for neowise yr3 data release

--- src/firefly/java/edu/caltech/ipac/firefly/data/WiseRequest.java
+++ src/firefly/java/edu/caltech/ipac/firefly/data/WiseRequest.java
@@ -112,7 +112,7 @@ public class WiseRequest extends TableServerRequest {
             put(POSTCRYO+"|1b",  new String[]{"2band_p1bm_frm", "2band_p1bs_psd"});
             put(MERGE+"|1b", new String[]{"merge_p1bm_frm", "merge_p1bs_psd"});
             put(MERGE+"|3a", new String[]{"merge_p3am_cdd", "merge_p3as_psd"});
-            put(NEOWISER +"|1b", new String[]{"p1bm_frm", "q1bs_psd"});
+            put(NEOWISER +"|1b", new String[]{"p1bm_frm", "p1bs_psd"});
 
             put(PASS1+"|1b", new String[]{"i1bm_frm", "i1bs_psd"});
             put(PASS1+"|3a", new String[]{"i3am_cdd", "i3as_psd"});
@@ -193,14 +193,14 @@ public class WiseRequest extends TableServerRequest {
             put(ALLSKY_4BAND,"allsky_4band_p1bs_psd");
             put(CRYO_3BAND,"allsky_3band_p1bs_psd");
             put(POSTCRYO,"allsky_2band_p1bs_psd");
-            put(NEOWISER,"neowiser_q1bs_psd");
+            put(NEOWISER,"neowiser_p1bs_psd");
             put(PASS1,"pass1_i1bs_psd_view");
             put(PASS2_4BAND,"pass2_4band_i1bs_psd_view");
             put(PASS2_3BAND,"pass2_3band_i1bs_psd_view");
             put(PASS2_2BAND, "pass2_2band_i1bs_psd_view");
             put(NEOWISER_YR1,"neowiser_p1bs_psd");
             put(NEOWISER_YR2,"neowiser_p1bs_psd");
-            put(NEOWISER_YR3,"neowiser_yr3_i1bs_psd");
+            put(NEOWISER_YR3,"neowiser_p1bs_psd");
             put(NEOWISER_YR4,"neowiser_i1bs_psd");
         }
     };
