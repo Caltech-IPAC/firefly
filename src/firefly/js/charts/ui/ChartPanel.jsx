184fcae07cc03ec4d92eb564be004fdfbe87356c
tatianag <tatianag@ipac.caltech.edu>
2017-06-13 12:55:41 -0700

    bug fix, which was causing LSST TS to fail

--- src/firefly/js/charts/ui/ChartPanel.jsx
+++ src/firefly/js/charts/ui/ChartPanel.jsx
@@ -259,7 +259,7 @@ export class ChartPanel extends PureComponent {
             if (!updateOnStoreChange || updateOnStoreChange(this.state)) {
                     this.setState(this.getNextState());
             }  else {
-                const {showOptions, optionsKey} = ChartsCntlr.getChartData(this.props.chartId);
+                const {showOptions, optionsKey} = ChartsCntlr.getChartData(this.props.chartId) || {};
                 if (showOptions !== this.state.showOptions || optionsKey !== this.state.optionsKey) {
                     this.setState({showOptions, optionsKey});
                 }
