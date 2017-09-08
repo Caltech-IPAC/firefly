d363f0bc7a73b151746d76ce503769b981a3967e
loi <loi@ipac.caltech.edu>
2017-09-08 10:54:08 -0700

    DM-11547:  changes based on reviwer comments - update proxyctl script to take additional parameters - added wget and telnet into image for debugging use - fixed properties without defualts which prevented apache from starting up - fixed static link to cm/firefly path

--- docker/proxy-dev/proxyctl.sh
+++ docker/proxy-dev/proxyctl.sh
@@ -1,4 +1,41 @@
-#!/bin/sh
+#!/bin/bash
+
+
+http_port="80"
+https_port="443"
+docker_host="`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'  | tail -1`"
+firefly_port="8080"
+
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -http|--http_port)
+    http_port="$2"
+    shift # past argument
+    ;;
+    -https|--https_port)
+    https_port="$2"
+    shift # past argument
+    ;;
+    -host|--docker_host)
+    docker_host="$2"
+    shift # past argument
+    ;;
+    -firefly|--firefly_port)
+    firefly_port="$2"
+    shift # past argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+
 case "$1" in
   start)
         docker start proxy-dev
@@ -13,15 +50,25 @@ case "$1" in
         docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
         ;;
   update)
-        cd /hydra/cm/firefly
+        echo ----------------------------------------
+        echo http_port  = "${http_port}"
+        echo https_port  = "${https_port}"
+        echo docker_host  = "${docker_host}"
+        echo firefly_port  = "${firefly_port}"
+        echo ----------------------------------------
+
+        DIR=$(dirname "${0}")
+        cd $DIR/../../../firefly
         gradle proxyDev:dockerImage
         docker stop proxy-dev
         docker container rm proxy-dev
 
         docker run -d \
-                   -p 80:80 \
-                   -p 443:443 \
-                   -e "docker_host=`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'  | tail -1`" \
+                   -p ${http_port}:80 \
+                   -p ${https_port}:443 \
+                   -e docker_host=${docker_host} \
+                   -e firefly_port=${firefly_port} \
+                   -e docker_host=`ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}'  | tail -1` \
                    --name proxy-dev ipac/proxy-dev
 
 #           -e "DOMAINS=`hostname`" \
@@ -30,7 +77,14 @@ case "$1" in
 
         ;;
   *)
-        echo $"Usage: proxyctl.sh [start|stop|shell|clean|update]"
-        exit
+        echo
+        echo "Usage: proxyctl.sh [start|stop|shell|clean|update]"
+        echo "  optional parameters for update only:"
+        echo "    -http|--http_port <number>"
+        echo "    -https|--https_port <number>"
+        echo "    -host|--docker_host <x.x.x.x>  : ip address of the host running firefly"
+        echo "    -firefly|--firefly_port <number>"
+        echo
+        exit 1
 esac
 
