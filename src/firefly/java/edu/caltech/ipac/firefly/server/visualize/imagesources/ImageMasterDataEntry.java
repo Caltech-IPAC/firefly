ce58c67ddf2064ee0f320533043b8180e15bdb6d
Lijun Zhang <zhang@boai.ipac.caltech.edu>
2017-10-05 11:54:38 -0700

    IRSA-816    Removed the IrsaParameter class, intead use enum type in ImageMasterDataEntry.  Thus all the parameter information is kept in the same file.

--- src/firefly/java/edu/caltech/ipac/firefly/server/visualize/imagesources/ImageMasterDataEntry.java
+++ src/firefly/java/edu/caltech/ipac/firefly/server/visualize/imagesources/ImageMasterDataEntry.java
@@ -12,35 +12,49 @@ import java.util.HashMap;
 import java.util.Map;
 /**
  * @author Trey Roby
+ * 10/5/17
+ * Modified by LZ
  */
 public class ImageMasterDataEntry {
 
     public static final String PLOT_REQUEST_PARAMS= "plotRequestParams";
     private Map<String,Object> map= new HashMap<>();
 
+    public enum PARAMS{
+        MIN_RANGE_DEG("minRangeDeg"),
+        MAX_RANGE_DEG("maxRangeDeg"),
+        PROJECT("project"),
+        SUB_PROJECT ("subProject"),
+        TITLE("title"),
+        WAVELENGTH ("wavelength"),
+        WAVELENGTH_DESC("wavelengthDesc"),
+        HELP_URL("helpUrl"),
+        TOOL_TIP ( "tooltip"),
+        IMAGE_ID ("imageId"),
+        PROJECT_TYPE_KEY("projectTypeKey"),
+        PROJECT_TYPE_DESC("projectTypeDesc"),
+        MISSION_ID("missionId"),
+        INSTRUMENT_ID("instrumentId"),
+        ACRONYM("acronym"),
+        NAME("name"),
+        DATA_TYPE("dataType");
 
-    public void setProject(String p) { map.put(IrsaMasterTableParameters.PROJECT, p);}
-    public void setSubProject(String p) { map.put(IrsaMasterTableParameters.SUB_PROJECT, p);}
-    public void setTitle(String t) { map.put(IrsaMasterTableParameters.TITLE, t);}
-    public void setWavelength(String wl) { map.put(IrsaMasterTableParameters.WAVELENGTH, wl);}
-    public void setWavelengthDesc(String desc) { map.put(IrsaMasterTableParameters.WAVELENGTH_DESC, desc);}
-    public void setHelpUrl(String url) { map.put(IrsaMasterTableParameters.HELP_URL, url);}
-    public void setTooltip(String tip) { map.put(IrsaMasterTableParameters.TOOL_TIP, tip);}
-    public void setProjectTypeKey(String key) { map.put(IrsaMasterTableParameters.PROJECT_TYPE_KEY, key);}
-    public void setProjectTypeDesc(String desc) { map.put(IrsaMasterTableParameters.PROJECT_TYPE_DESC, desc);}
-    public void setMinRangeDeg(String minRangeDeg) { map.put(IrsaMasterTableParameters.MIN_RANGE_DEG, minRangeDeg);}
-    public void setMaxRangeDeg(String maxRangeDeg) { map.put(IrsaMasterTableParameters.MAX_RANGE_DEG, maxRangeDeg);}
+        private String key;
+        PARAMS(String key){
+            this.key=key;
+        }
+
+        String getKey(){
+            return this.key;
+        }
+
+    }
+
     public void setPlotRequestParams(Map<String,String> params) { map.put(PLOT_REQUEST_PARAMS, params);}
-    public void setMissionId(String missionId) { map.put(IrsaMasterTableParameters.MISSION_ID, missionId);}
-    public void setInstrumentId(String instrumentId) { map.put(IrsaMasterTableParameters.INSTRUMENT_ID, instrumentId);}
-    public void setAcronym(String acry) { map.put(IrsaMasterTableParameters.ACRONYM , acry);}
-    public void setDataType(String type) { map.put(IrsaMasterTableParameters.DATA_TYPE, type);}
-    public void setAtLasTable(String atlas) { map.put(IrsaMasterTableParameters.ATLAS_TABLE, atlas);}
-    public void setImageId(String id) { map.put(IrsaMasterTableParameters.IMAGE_ID, id);}
 
 
+    public void set(PARAMS key, String val){map.put(key.getKey(), val);}
 
-    public void set(String key, String val){map.put(key, val);}
 
     public Map<String,Object> getDataMap() {
         return map;
--- src/firefly/java/edu/caltech/ipac/firefly/server/visualize/imagesources/IrsaMasterDataSource.java
+++ src/firefly/java/edu/caltech/ipac/firefly/server/visualize/imagesources/IrsaMasterDataSource.java
@@ -7,24 +7,27 @@ package edu.caltech.ipac.firefly.server.visualize.imagesources;
 
 import edu.caltech.ipac.firefly.server.util.DsvToDataGroup;
 import edu.caltech.ipac.firefly.server.util.Logger;
+import edu.caltech.ipac.util.AppProperties;
 import edu.caltech.ipac.util.DataGroup;
 import edu.caltech.ipac.util.DataObject;
 import edu.caltech.ipac.util.download.FailedRequestException;
 import org.apache.commons.csv.CSVFormat;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import edu.caltech.ipac.firefly.server.visualize.imagesources.ImageMasterDataEntry.PARAMS;
 
 /**
  * @author Trey Roby
+ * 10/05/17 LZ modified and added some methods
  */
 public class IrsaMasterDataSource implements ImageMasterDataSourceType {
-    private final String masterTable = "/edu/caltech/ipac/firefly/resources/irsa-master-table.csv";
+    private final static String masterTable = "/edu/caltech/ipac/firefly/resources/irsa-master-table.csv";
     private static final Logger.LoggerImpl LOG = Logger.getLogger();
+    private static final String IRSA_MASTER_TABLE = AppProperties.getProperty("irsa.mastertable.location",masterTable);
 
 
 
@@ -32,9 +35,10 @@ public class IrsaMasterDataSource implements ImageMasterDataSourceType {
     public List<ImageMasterDataEntry> getImageMasterData() {
         try {
 
-            return createDataList(masterTable);
+            return createDataList(IRSA_MASTER_TABLE);
 
         } catch (FailedRequestException  | IOException e) {
+            LOG.info("Reading IRSA Master table failed");
             e.printStackTrace();
             return null;
         } catch (Exception e) {
@@ -48,7 +52,6 @@ public class IrsaMasterDataSource implements ImageMasterDataSourceType {
 
         HashMap<String, String> paramMaps = new HashMap<>();
         paramMaps.put("ProjectTypeDesc", "type");
-
         paramMaps.put("apiType", "Service");
         paramMaps.put("dataProductId", "surveyKey");
         paramMaps.put("waveBandId", "surveyKeyBand");
@@ -56,53 +59,55 @@ public class IrsaMasterDataSource implements ImageMasterDataSourceType {
         paramMaps.put("filter", "filter");
         return paramMaps;
     }
-    private String[] getDefinedParameters() throws IOException, FailedRequestException{
-        Field[] fields = IrsaMasterTableParameters.class.getDeclaredFields();
-        String[] keys = new String[fields.length];
 
-        for (int i=0; i<fields.length; i++){
-            try {
-                keys[i] = (String) fields[i].get(null);
-            }
-            catch (IllegalAccessException x) {
-                continue; // not a publicly accessible field
-            } catch (NullPointerException x) {
-                continue; // non-static field
-            }
-        }
-        return keys;
-    }
 
-    private boolean validateMasterTable(DataGroup inDg,  String[] parameterKeys){
-        for (int i=0; i<parameterKeys.length; i++){
-            if (!inDg.containsKey(parameterKeys[i])){
+    private boolean validateMasterTable(DataGroup inDg,  PARAMS[] parameters){
+
+        for (int i=0; i<parameters.length; i++){
+            if (!inDg.containsKey(parameters[i].getKey())){
+                LOG.info("The master table does not contains "+parameters[i].getKey());
                 return false;
             }
         }
         return true;
     }
-      /**
-       * This method reads in the master-table and then create a Map List based on the table data.
-       * @return
-       * @throws IOException
-       * @throws FailedRequestException
-       */
+
+    /**
+    * This method reads in the master-table and then create a Map List based on the table data.
+    * @return
+    * @throws IOException
+    * @throws FailedRequestException
+    */
      public List createDataList(String masterTable) throws Exception {
          List<ImageMasterDataEntry>  retList= new ArrayList();
          DataGroup inDg = getDataFromMasterTable(masterTable);
          List<DataObject> dataRows = inDg.values();
          Map<String, String> paramMaps=getParameterMaps();
-         String[] parameterKeys = getDefinedParameters();
-         if (!validateMasterTable(inDg, parameterKeys)){
+         PARAMS[] parameters = ImageMasterDataEntry.PARAMS.values();
+
+         ArrayList<String> missionIdList=new ArrayList<>();
+         if (!validateMasterTable(inDg, parameters)){
              throw new Exception("The column names do not match the required parameter names");
          }
          for(DataObject row : dataRows) {
              ImageMasterDataEntry entry = new ImageMasterDataEntry();
-             for (int i=0; i<parameterKeys.length; i++){
-                 Object obj = row.getDataElement(parameterKeys[i]);
-                 String val = obj!=null?String.valueOf(obj):null;
-                 entry.set(parameterKeys[i], val );
+             for (int i=0; i<parameters.length; i++){
 
+                     Object obj = row.getDataElement(parameters[i].getKey());
+                     String val = obj != null ? String.valueOf(obj) : null;
+
+                     //make a temporary missionId
+                     if (val==null && parameters[i].getKey().equals("missionId")){
+                         val = "m_"+String.valueOf(row.getDataElement("acronym"))+
+                                 String.valueOf(row.getDataElement("waveBandId"))+
+                                 String.valueOf(row.getDataElement("wavelength"));
+                         if( missionIdList.contains(val)){
+                             val=val.concat(new String("_"+i));
+                         }
+                         missionIdList.add(val);
+                     }//end making missionId
+
+                     entry.set(parameters[i], val);
              }
              entry.setPlotRequestParams(getMappedPlotRequestParam(row, paramMaps));
 
