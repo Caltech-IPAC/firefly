db0c402a7e59d185fb8a07325df46b37683283a7
Lijun Zhang <zhang@boai.ipac.caltech.edu>
2018-04-11 15:33:57 -0700

    IRSA-1519   Fixed the barnard's star search issue.  The changes are in the server side.

--- src/firefly/js/ui/TargetPanelWorker.js
+++ src/firefly/js/ui/TargetPanelWorker.js
@@ -6,7 +6,7 @@
 import PositionParser from '../util/PositionParser';
 import PositionFieldDef from '../data/form/PositionFieldDef';
 import {getRootPath} from '../util/BrowserUtil.js';
-import {fetchUrl,encodeUrl} from '../util/WebUtil.js';
+import {fetchUrl} from '../util/WebUtil.js';
 import {parseWorldPt} from '../visualize/Point';
 
 
