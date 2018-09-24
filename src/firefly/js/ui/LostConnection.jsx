15833017260b3c2976463447ec2ddf961bb0ada7
loi <loi@ipac.caltech.edu>
2018-09-24 09:04:38 -0700

    DM-15145: change id name from 'fireflyWarn' to 'fireflyLostConnWarn'

--- src/firefly/js/ui/LostConnection.jsx
+++ src/firefly/js/ui/LostConnection.jsx
@@ -10,27 +10,26 @@ import {getWsInfo} from '../core/AppDataCntlr.js';
 import {dispatchAddActionWatcher} from '../core/MasterSaga';
 import {APP_UPDATE} from '../core/AppDataCntlr';
 
-export const warningDivId = 'fireflyWarn';
+export const warningDivId = 'fireflyLostConnWarn';
 
 /**
- * The lost-connection will attach itself to a div with an id that starts with 'fireflyWarn'.
+ * When connection is lost, a warning indicator will attach itself to a div with an id that starts with 'fireflyLostConnWarn'.
  * This means there can be more than one.  However, if one is not detected, it will do nothing.
- * When constructing the div, you may also provide a 'data-decor' attribute.  This will affect how the warning looks.
- *      <div id="fireflyWarn" data-decor="small" class="warning-div center"></div>
+ * When constructing the div, a 'data-decor' attribute can be set to affect how the warning look.
+ *      <div id="fireflyLostConnWarn" data-decor="small" class="warning-div center"></div>
  *
- * Currently, we support 'data-decor' or 'small', 'medium', and 'full' with 'medium' as the default.
+ * Currently, we support 'data-decor' of 'small', 'medium', and 'full' with 'medium' as the default.
  *  small   : 24x24 transparent icon with tooltips
  *  medium  : 48x48 transparent icon with tooltips
  *  full    : 48x48 transparent icon with the message visible
  *
- * For conveniences, there are 3 defined styles one can apply to a fireflyWarn div:
+ * For conveniences, there are 3 defined styles to layout the fireflyLostConnWarn div:
  *   warning-div right:    horizontally right-align with a z-index of 300
  *   warning-div left:     horizontally left-align with a z-index of 300
  *   warning-div center:   horizontally center-align with a z-index of 300
  *
- *
  * See src/firefly/html/demo/ffapi-table-test.html for an example of multiple warning is different styles.
- * There is one on top center and 3 on the bottom.  Search for div with id starting with 'fireflyWarn'.
+ * There is one on top center and 3 on the bottom.  Search for div with id starting with 'fireflyLostConnWarn'.
  *
  */
 export function initLostConnectionWarning() {
@@ -39,7 +38,7 @@ export function initLostConnectionWarning() {
     const onConnectionUpdate = (action) => {
         const {payload} = action || {};
         if (! get(payload, 'websocket.isConnected', true)) {
-            const divElements = document.querySelectorAll('[id^="fireflyWarn"]');
+            const divElements = document.querySelectorAll('[id^="fireflyLostConnWarn"]');
             if (divElements) {
                 divElements.forEach( (div) => {
                     ReactDOM.unmountComponentAtNode(div);        // in case one is already mounted.
