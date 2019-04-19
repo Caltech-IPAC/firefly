391526860aa92b8146a223f049e3c6bf0b692f33
cwang2016 <cwang@ipac.caltech.edu>
2019-04-19 08:43:08 -0700

    FIREFLY-22 update set function in PrimitiveList for Integers class per review comments

--- src/firefly/java/edu/caltech/ipac/table/PrimitiveList.java
+++ src/firefly/java/edu/caltech/ipac/table/PrimitiveList.java
@@ -6,7 +6,6 @@ package edu.caltech.ipac.table;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Date: 6/15/18
@@ -21,32 +20,22 @@ public interface PrimitiveList {
     int size();
     void clear();
     void trimToSize();
-    List<Class> numberTypeList = Arrays.asList(new Class[] {Byte.class, Short.class, Integer.class});
 
     default void add(Object val) {
         set(size(), val);
     }
 
     default void checkType(Object val) {
-        int vIdx = val == null ? -1 : numberTypeList.indexOf(val.getClass());
-        int dIdx = numberTypeList.indexOf(getDataClass());
-
-        // number type value or non number type value
-        if ((vIdx >= 0 && dIdx >= 0 && vIdx > dIdx) ||
-            (vIdx < 0 && val != null && !val.getClass().isAssignableFrom(getDataClass()))) {
+        if (val != null && !val.getClass().isAssignableFrom(getDataClass())) {
             throw new RuntimeException(String.format("Type mismatch(%s): expecting %s but found %s", val, getDataClass(), val.getClass()));
         }
     }
 
     default int getIntValue(Object val) {
-        int toIdx = numberTypeList.indexOf(Integer.class);
-        int fromIdx = numberTypeList.indexOf(val.getClass());
-
-        if (toIdx == fromIdx) {
-            return ((Integer) val).intValue();
-        } else {
-            return (toIdx == 0) ? ((Byte) val).intValue() : ((Short) val).intValue();
-        }
+        if (val instanceof Integer) return (int)val;
+        else if (val instanceof Byte) return (int)(Byte)val;
+        else if (val instanceof Short) return (int)(Short)val;
+        throw new RuntimeException(String.format("%s is with type %s, can not be assigned to Integer", val, getDataClass()));
     }
 
 
@@ -239,9 +228,9 @@ public interface PrimitiveList {
         }
 
         public void set(int idx, Object val) {
-            checkType(val);
+            int ival = val == null ? Integer.MIN_VALUE : getIntValue(val);
             ensureCapacity(idx);
-            data[idx] = val == null ? Integer.MIN_VALUE : getIntValue(val);
+            data[idx] = ival;
             if (idx >= size()) size = idx+1;
         }
 
