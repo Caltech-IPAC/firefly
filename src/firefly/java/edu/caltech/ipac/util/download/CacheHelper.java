1ba59c1b1cb3058ca20e6721521ad23bc14d797a
roby <roby@ipac.caltech.edu>
2022-02-15 10:57:01 -0700

    firefly-841: check for directory recreate

--- src/firefly/java/edu/caltech/ipac/util/download/CacheHelper.java
+++ src/firefly/java/edu/caltech/ipac/util/download/CacheHelper.java
@@ -11,7 +11,7 @@ import edu.caltech.ipac.util.cache.CacheManager;
 import java.io.File;
 
 /**
- * This class is provide an interface for classes that use both server and client cache.  One will use one when running
+ * This class is provides an interface for classes that use both server and client cache.  One will use one when running
  * on the server and the other when running as a client.
  * @author Trey Roby
  */
@@ -25,7 +25,10 @@ public class CacheHelper {
     public static File makeFile(String name) { return new File(getDir(),name); }
     public static File makeFile(File dir, String name) { return new File(dir!=null ? dir : getDir(),name); }
 
-    public static File getDir() { return _cacheDir; }
+    public static File getDir() {
+        if (!_cacheDir.exists()) _cacheDir.mkdirs();
+        return _cacheDir;
+    }
 
     public static void putFileInfo(CacheKey key, FileInfo fileInfo) { fileCache.put(key,fileInfo); }
 
