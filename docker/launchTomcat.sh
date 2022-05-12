c7bb3a0a2225b28bdf4137ca87690e521306caca
loi <loi@ipac.caltech.edu>
2022-05-12 16:19:59 -0700

    fix java module illegal-access exceptions

--- docker/launchTomcat.sh
+++ docker/launchTomcat.sh
@@ -109,11 +109,8 @@ export CATALINA_OPTS="\
 # Java 9 introduces Modularity with module level security
 # Firefly apps requires these module to be opened
 CATALINA_OPTS="$CATALINA_OPTS \
-    --add-opens java.base/java.util=ALL-UNNAMED \
-    --add-opens java.base/java.text=ALL-UNNAMED \
-    --add-opens java.desktop/java.awt.font=ALL-UNNAMED \
-    "
-
+    --illegal-access=warn \
+  "
 
 
 #------- start background scripts: cleanup
