39eb6e19877153e4c6287bf4601e59979342a9c6
loi <loi@ipac.caltech.edu>
2022-05-23 17:06:47 -0700

    merge changes from dev over to gwt

--- docker/launchTomcat.sh
+++ docker/launchTomcat.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
 NAME=${BUILD_TIME_NAME:-"ipac/firefly"}
-if [ -z "${MAX_JVM_SIZE}" ]; then
-  MM_NOT_USED="not used"
-  MIN_JVM_SIZE=
-else
-  PER_NOT_USED="not used"
-  INIT_RAM_PERCENT=
-  MAX_RAM_PERCENT=
-fi
-
 ADMIN_PASSWORD=${ADMIN_PASSWORD:-`echo $RANDOM | base64 | head -c 8`}
 
 echo -e "\n!!============================================================"
@@ -33,15 +24,9 @@ echo "        Additional data path             VISUALIZE_FITS_SEARCH_PATH ${VISU
 echo "        Clean internal(eg- 720m, 5h, 3d) CLEANUP_INTERVAL           ${CLEANUP_INTERVAL}"
 echo
 echo "Advanced environment variables:"
-echo "        Min JVM size (*)                 MIN_JVM_SIZE               ${MIN_JVM_SIZE}${MM_NOT_USED}"
-echo "        Max JVM size (*)                 MAX_JVM_SIZE               ${MAX_JVM_SIZE}${MM_NOT_USED}"
-echo "        Initial JVM size (**)            INIT_RAM_PERCENT           ${INIT_RAM_PERCENT}${PER_NOT_USED}"
-echo "        Max JVM size (**)                MAX_RAM_PERCENT            ${MAX_RAM_PERCENT}${PER_NOT_USED}"
-echo "        CPU cores (0 means guess)        JVM_CORES                  ${JVM_CORES}"
 echo "        Run tomcat with debug            DEBUG                      ${DEBUG}"
-echo "        Extra firefly properties         FIREFLY_OPTS               ${FIREFLY_OPTS}"
-echo "  (*)  If MAX_JVM_SIZE is blank, autosizing properties INIT_RAM_PERCENT and MAX_RAM_PERCENT are used instead"
-echo "  (**) Autosizing properties INIT_RAM_PERCENT and MAX_RAM_PERCENT are not used if MAX_JVM_SIZE is set"
+echo "        Extra firefly properties(*)      PROPS                      ${PROPS}"
+echo " (*) key=value pairs separated by spaces"
 echo
 echo "Ports: "
 echo "        8080 - http"
@@ -77,12 +62,6 @@ if [ "$1" = "--help" ] || [ "$1" = "-help" ] || [ "$1" = "-h" ]; then
     exit 0
 fi
 
-if [ -z ${MAX_JVM_SIZE} ]; then
-   JVM_SIZING="-XX:InitialRAMPercentage=${INIT_RAM_PERCENT} -XX:MaxRAMPercentage=${MAX_RAM_PERCENT}"
-else
-   JVM_SIZING="-Xms${MIN_JVM_SIZE} -Xmx${MAX_JVM_SIZE}"
-fi
-
 if [ -z ${VISUALIZE_FITS_SEARCH_PATH} ]; then
    VIS_PATH="/external"
 else
@@ -91,7 +70,8 @@ fi
 
 #---------   CATALINA_OPTS must be exported for catalina.sh to pick them up
 export CATALINA_OPTS="\
-  ${JVM_SIZING} \
+  -XX:InitialRAMPercentage=${INIT_RAM_PERCENT} \
+  -XX:MaxRAMPercentage=${MAX_RAM_PERCENT} \
   -DADMIN_USER=${ADMIN_USER} \
   -DADMIN_PASSWORD=${ADMIN_PASSWORD} \
   -Dhost.name=${HOSTNAME} \
@@ -106,19 +86,19 @@ export CATALINA_OPTS="\
   -Dvisualize.fits.search.path=${VIS_PATH} \
 	"
 
-#----- eval FIREFLY_OPTS if exists.  key-value pairs are separated by spaces. therefore, it does not support values with spaces in it.
-if [ ! -z "${FIREFLY_OPTS}" ]; then
-  jvmProps=`sed -r 's/-D//g;s/( )+/ -D/g;s/^/-D/' <<< $FIREFLY_OPTS`     # remove -D if exists(backward compatible), then add -D to every pair
+#----- eval PROPS if exists.  key-value pairs are separated by spaces. therefore, it does not support values with spaces in it.
+if [ ! -z "${PROPS}" ]; then
+  jvmProps=`sed -r 's/( )+/ -D/g;s/^/-D/' <<< $PROPS`     # add -D to every key=val pair
   export CATALINA_OPTS="${CATALINA_OPTS} ${jvmProps}"
 fi
-# envVar with names matching 'FIREFLY_OPTS_*'.
+# envVar with names matching 'PROPS_*'.
 # A more advanced internal scheme to support secrets, quotes, and spaces in values
 # Use '__' in key, to sub for '.' since '.' is not allowed in envVar.
-for var in "${!FIREFLY_OPTS_@}"; do
-  prop=`sed 's/FIREFLY_OPTS_//g;s/__/./g' <<< "$var"`
+for var in "${!PROPS_@}"; do
+  prop=`sed 's/PROPS_//g;s/__/./g' <<< "$var"`
   export CATALINA_OPTS="${CATALINA_OPTS} -D$prop=${!var}"
 done
-#----- eval FIREFLY_OPTS
+#----- eval PROPS
 
 # Java 9 introduces Modularity with module level security
 # GWT apps requires these module to be opened
