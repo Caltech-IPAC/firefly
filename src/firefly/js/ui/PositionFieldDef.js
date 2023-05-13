9b110747aed9d8a8c0102dc7cd11fd5142d693c3
kpuriIpac <kpuri@ipac.caltech.edu>
2023-05-12 17:37:22 -0700

    FIREFLY-1234:convert all non EQJ2000 coords to EQJ2000 coords

--- src/firefly/js/ui/PositionFieldDef.js
+++ src/firefly/js/ui/PositionFieldDef.js
@@ -3,6 +3,8 @@ import CoordinateSys from '../visualize/CoordSys.js';
 import CoordUtil from '../visualize/CoordUtil.js';
 
 import {sprintf} from '../externalSource/sprintf.js';
+import VisUtil from 'firefly/visualize/VisUtil';
+import {instanceOf} from "prop-types";
 
 const errMsgRoot= 'Error: ';
 
@@ -61,11 +63,24 @@ export function coordToString(csys) {
 export function formatTargetForHelp(wp) {
     if (!wp) return '';
     if (!wp.objName || !wp.resolver) return formatPosForHelp(wp);
-    return ' <b>' + wp.objName + '</b>' +
-        ' <i>resolved by</i> ' + wp.resolver.desc +
-        '<div  style=\"padding-top:6px;\">' +
-        formatPosForHelp(wp) +
-        '</div>';
+    return `<b>${wp.objName}</b> <i>resolved by</i> ${wp.resolver.desc}
+            <div style="padding-top:6px;">
+              ${formatPosForHelp(wp)}
+            </div>`;
+}
+
+function getEQJ2000(wp) {
+    const wpEQ = VisUtil.convert(wp, CoordinateSys.EQ_J2000);
+    if (typeof wpEQ === 'undefined') return '';
+    const [ra, dec] = [wpEQ.getLon().toFixed(5), wpEQ.getLat().toFixed(5)];
+    const hmsRa = CoordUtil.convertLonToString(ra,  CoordinateSys.EQ_J2000);
+    const hmsDec = CoordUtil.convertLatToString(dec,  CoordinateSys.EQ_J2000);
+    const res = `<div style="padding-top:6px;font-size:10px;">
+                  ${ra},&nbsp;${dec}&nbsp;Equ J2000
+                  &nbsp;&nbsp;&nbsp;<i>or</i> &nbsp;&nbsp;&nbsp;
+                  ${hmsRa},&nbsp;${hmsDec}&nbsp;&nbsp;Equ J2000
+                </div>`;
+    return res;
 }
 
 /**
@@ -80,19 +95,23 @@ export function formatPosForHelp(wp) {
     const latStr = sprintf('%.5f',wp.getLat());
     const csys = coordToString(wp.getCoordSys());
     if (wp.getCoordSys().isEquatorial()) {
-
         const hmsRa = CoordUtil.convertLonToString(wp.getLon(), wp.getCoordSys());
         const hmsDec = CoordUtil.convertLatToString(wp.getLat(), wp.getCoordSys());
 
-        s = '<div class=\"on-dialog-help faded-text\" style=\"font-size:10px;\">' +
-            lonStr + ',&nbsp;' + latStr + '&nbsp;&nbsp;' + csys +
-            ' &nbsp;&nbsp;&nbsp;<i>or</i> &nbsp;&nbsp;&nbsp;' +
-            hmsRa + ',&nbsp;' + hmsDec + '&nbsp;&nbsp;' + csys +
-            '</div>';
+        s = `<div class="on-dialog-help faded-text" style="font-size:10px;">
+              ${lonStr},&nbsp;${latStr}&nbsp;&nbsp;${csys}
+              &nbsp;&nbsp;&nbsp;<i>or</i> &nbsp;&nbsp;&nbsp;
+              ${hmsRa},&nbsp;${hmsDec}&nbsp;&nbsp;${csys}
+            </div>`;
+
+        if (wp.getCoordSys() != CoordinateSys.EQ_J2000) {
+            s += getEQJ2000(wp);
+        }
     }
     else {
-        s = '<div class=on-dialog-help>' +
-            lonStr + ',&nbsp;' + latStr + '&nbsp;&nbsp;' + csys + '</div>';
+        s = `<div class=\"on-dialog-help faded-text\" style=\"font-size:10px;\"> 
+            ${lonStr},&nbsp;${latStr}&nbsp;&nbsp;${csys}</div>`;
+        s += getEQJ2000(wp);
     }
     return s;
 
