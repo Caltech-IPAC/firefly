5be6f4b597029849e63a50ef8e69d4e7f9c1bd3b
wmi <wmi@ipac.caltech.edu>
2023-08-10 16:15:10 -0700

    IRSA-5419 Stop use gwt sha host, using rootURL instead for servlet call point data server to irsadev for test

--- src/firefly/js/metaConvert/ShaRequestList.js
+++ src/firefly/js/metaConvert/ShaRequestList.js
@@ -10,10 +10,11 @@ import {CoordinateSys} from '../visualize/CoordSys.js';
 import {WebPlotRequest} from '../visualize/WebPlotRequest.js';
 import {addCommonReqParams} from '../templates/lightcurve/LcConverterFactory.js';
 import {GRID_FULL, SINGLE} from '../visualize/MultiViewCntlr';
+import {getRootURL} from '../util/WebUtil.js';
 
 const colToUse= ['reqkey', 'heritgefilename', 'fname'];
 const rangeValues= RangeValues.makeRV({which:SIGMA, lowerValue:-2, upperValue:10, algorithm:STRETCH_LINEAR});
-
+const baseUrl = 'https://irsadev.ipac.caltech.edu';
 /**
  * make a list of plot request for wise. This function works with ConverterFactory.
  * @param table
@@ -44,8 +45,9 @@ export function makeShaPlotRequest(table, row, includeSingle,includeStandard) {
     /*
      const url = https://irsa.ipac.caltech.edu/data/SPITZER/SHA/archive/proc/MIPS003600/r5572864/ch2/bcd/SPITZER_M2_5572864_0007_0061_9_bcd.fits
      */
-    const serverinfo = 'https://irsa.ipac.caltech.edu/data/SPITZER/';
-    const mosimgserver = 'https://sha.ipac.caltech.edu/applications/Spitzer/SHA/servlet/ProductDownload?DATASET=level1';
+    const serverinfo = 'https://irsadev.ipac.caltech.edu/data/SPITZER/';    //for test purpose
+    //const shahost = 'https://sha.ipac.caltech.edu/'; //gwt sha host
+    const shaservlet = '/applications/Spitzer/SHA/servlet/ProductDownload?DATASET=level1';
     let filepath = '';
     let url = '';
     let wp;
@@ -56,12 +58,13 @@ export function makeShaPlotRequest(table, row, includeSingle,includeStandard) {
     } else if (dataType === 'SEIP') {
         url = `${serverinfo}` + 'Enhanced/SEIP/' + `${dataFile}`;
     } else if (dataType === 'PRECOVERY') {
-        url = `${mosimgserver}` + '&ID=' + `${bcdId}`;
+        url += `${shaservlet}` + '&ID=' + `${bcdId}`;
+        url = new URL(url, getRootURL());
     } else {
         filepath = dataFile.replaceAll('/sha', 'SHA');
         url = `${serverinfo}/${filepath}`;
     }
-    
+
 
     if (dataType === 'PRECOVERY') {
         wp = makeWorldPt(ra_obj, dec_obj, CoordinateSys.EQ_J2000);
