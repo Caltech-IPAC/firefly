968e9b0431e4c5e128e8b8a9e81a74a74ccd1772
wmi <wmi@ipac.caltech.edu>
2024-06-20 11:57:15 -0700

    IRSA-5367 add where clause for refby sourceId search

--- src/firefly/java/edu/caltech/ipac/astro/ibe/datasource/WiseIbeDataSource.java
+++ src/firefly/java/edu/caltech/ipac/astro/ibe/datasource/WiseIbeDataSource.java
@@ -263,15 +263,17 @@ public class WiseIbeDataSource extends BaseIbeDataSource {
                 //queryParam.setRefBy(sourceSpec);
                 String scanId = refSourceId.trim().substring(0,6);
                 String framNum = String.valueOf(Integer.parseInt(refSourceId.trim().substring(6,9)));
-                String ref1B =  "scan_id=" + "\'"+ scanId + "\'"+"+AND+" + "frame_num="+"\'"+framNum+"\'";
-                queryParam.setRefBy(sourceSpec+"&where="+ref1B);
+                String ref1B =  "scan_id=" + "\'"+ scanId + "\'"+" AND " + "frame_num="+"\'"+framNum+"\'";
+                //queryParam.setRefBy(sourceSpec+"&where="+ref1B);
+                queryParam.setWhere(ref1B);
             } else if (sourceProductLevel == "3a") {
                 String coaddId = refSourceId.trim().substring(0,13);
                 DataProduct sourcedt = DataProduct.ALLSKY_4BAND_3A;
                 String sourceTable = sourcedt.getSourceTable();
                 String sourceSpec = WISE + "." + sourceTable + "(\"source_id\":\"" + refSourceId + "\")";
                 String ref3A = "coadd_id="+"\'"+coaddId+"\'";
-                queryParam.setRefBy(sourceSpec+"&where=" + ref3A);
+                //queryParam.setRefBy(sourceSpec+"&where=" + ref3A);
+                queryParam.setWhere(ref3A);
             }
         }
 
