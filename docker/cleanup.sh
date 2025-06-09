0a8f8095fee44afb12ad261ff8de62f35a82b5e3
loi <loi@ipac.caltech.edu>
2025-06-09 12:59:28 -0700

    Update the cleanup script so that it only targets its designated work directories.

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -39,22 +39,31 @@ if  ! [[ "${interval}" =~ $allDigitsRE ]] || [ "$mult" -eq 0 ]; then  # check fo
 fi
 
 cleanupMinutes=$((interval*mult))
+logfileMaxAge=$((cleanupMinutes*10+5))
 
 function doCleanup() {
     workarea="${1}"
+    log_dir="${workarea}/cleanup_logs"
+    mkdir -p "${log_dir}"
+
+    # cleanup old log files
+    ${find_cmd} "${log_dir}" -type f -mmin +${logfileMaxAge} -exec /bin/rm '{}' \+
+
+    # cleanup old work files
     timestamp=$(date +20%y%m%dT%H%M%S)
-    log_file="${workarea}/cleanup_logs/cleanup.${timestamp}.log"
-    clean_dirs=("${workarea}/temp_files" "${workarea}/visualize/fits-cache" "${workarea}/upload" "${workarea}/visualize/users")
+    log_file="${log_dir}/cleanup.${timestamp}.log"
+    clean_dirs=("${workarea}/temp_files" "${workarea}/visualize/fits-cache" "${workarea}/visualize/users")
     dirs_to_clear=("${workarea}/visualize/users" "${workarea}/temp_files")
     echo 'Cleanup, log_file: ' "${log_file}"
     {
         echo "Cleaning up work files older that ${cleanupMinutes} minutes, dir: ${workarea}"
         [[ -d "${workarea}/HiPS" ]] && ${find_cmd} "${workarea}/HiPS" -type f -mtime +90 -exec /bin/rm '{}' \+ -print
         [[ -d "${workarea}/stage" ]] && ${find_cmd} "${workarea}/stage" -type f -mtime +7 -exec /bin/rm '{}' \+ -print
-        [[ -d "${workarea}/perm_files" ]] && ${find_cmd} "${workarea}/perm_files" -type f -mtime +1 -exec /bin/rm '{}' \+ -print
+        [[ -d "${workarea}/upload" ]] && ${find_cmd} "${workarea}/upload" -type f -mtime +7 -exec /bin/rm '{}' \+ -print
+        [[ -d "${workarea}/perm_files" ]] && ${find_cmd} "${workarea}/perm_files" -type f -atime +1 -exec /bin/rm '{}' \+ -print
         for dir in "${clean_dirs[@]}"; do
            if [ -d "${dir}" ]; then
-              ${find_cmd} "${dir}" -type f -mmin +${cleanupMinutes} -exec /bin/rm '{}' \+ -print
+              ${find_cmd} "${dir}" -type f -amin +${cleanupMinutes} -exec /bin/rm '{}' \+ -print
            fi
         done
         for dir in "${dirs_to_clear[@]}"; do    # remove empty directories excluding those at the starting level
@@ -63,25 +72,29 @@ function doCleanup() {
      } > "${log_file}" 2>&1
 }
 
-logfileMaxAge=$((cleanupMinutes*10+5))
 sleepTime="$((cleanupMinutes+1))m"
 echo "Cleanup: started, interval time:" ${CLEANUP_INTERVAL}, "(${cleanupMinutes} minutes), sleepTime: ${sleepTime}"
+
+# Predefined directories
+workarea_dir="/firefly/workarea"
+shared_workarea="/firefly/shared-workarea"
 
 while true; do
     # Remove temporary products for each Firefly workarea
     sleep ${sleepTime}
-    for workarea_dir in "$@"; do
-        app_dirs=$(${find_cmd} "${workarea_dir}" -mindepth 1 -type d -prune)
-        for app_dir in ${app_dirs}; do
-            log_dir="${app_dir}/cleanup_logs"
-            if [ ! -d "${log_dir}" ]; then
-                mkdir -p "${log_dir}"
-            fi
-            # Remove old cleanup log files
-            ${find_cmd} "${log_dir}" -type f -mmin +${logfileMaxAge} -exec /bin/rm '{}' \+
-            if [ -d "${workarea_dir}" ]; then
-                doCleanup "${app_dir}"
-            fi
-        done
+
+    # Find app directories (should be only one, but loop for safety)
+    app_workdirs=$(${find_cmd} "${workarea_dir}" -mindepth 1 -type d -prune)
+
+    for work_dir in ${app_workdirs}; do
+        doCleanup "${work_dir}"
+
+        # Handle matching shared-workarea directory, if it exists
+        app_name="${work_dir#${workarea_dir}/}"
+        shared_work_dir="${shared_workarea}/${app_name}"
+        if [ -d "${shared_work_dir}" ]; then
+            doCleanup "${shared_work_dir}"
+        fi
+
     done
 done
