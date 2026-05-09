2e27ef2e9672cfb7d5efa09971bc45d9519f2853
Kartik Puri <kpuri@ipac.caltech.edu>
2026-05-08 19:38:05 -0700

    FIREFLY-2001: Fix embedded position collapse bug

--- src/firefly/js/visualize/ui/CommonUIKeys.js
+++ src/firefly/js/visualize/ui/CommonUIKeys.js
@@ -2,6 +2,7 @@ export const CONE_CHOICE_KEY = 'CONE';
 export const POLY_CHOICE_KEY = 'POLY';
 export const BOX_CHOICE_KEY = 'BOX';
 export const UPLOAD_CHOICE_KEY = 'UPLOAD';
+export const POINT_CHOICE_KEY = 'POINT';
 export const CONE_AREA_OPTIONS = [{label: 'Cone', value: CONE_CHOICE_KEY}, {label: 'Polygon', value: POLY_CHOICE_KEY}];
 
 export const CONE_AREA_OPTIONS_UPLOAD = [{label: 'Cone', value: CONE_CHOICE_KEY}, {label: 'Polygon', value: POLY_CHOICE_KEY},
