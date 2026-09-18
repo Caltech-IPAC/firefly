130c80e3788781e6a00379ba7ec7d8a00f3722a2
Alejandro Ventura De Moya <aventura@ipac.caltech.edu>
2026-09-18 06:35:44 -0700

    Firefly-1259: fix table header row scroll bug

--- src/firefly/js/tables/ui/BasicTableView.jsx
+++ src/firefly/js/tables/ui/BasicTableView.jsx
@@ -40,6 +40,10 @@ const tableStyleOverrides = {
     '.fixedDataTableRowLayout_rowWrapper': {
         fontSize: '12px',
     },
+    // Remove scroll container so the browser can't scroll the header and rows out of sync.
+    '.fixedDataTableRowLayout_main': {
+        overflow: 'clip',
+    },
     '.fixedDataTableCellLayout_main': {
         borderStyle: 'solid',
         borderWidth: '0 1px 1px 0',
